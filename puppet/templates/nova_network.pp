0fc680ffafad88c0525555221f9211c73acff6e4
Derek Higgins <derekh@redhat.com>
2012-08-24 08:23:28 +0100

    Adding support for specifying fixed and floating ip ranges

--- puppet/templates/nova_network.pp
+++ puppet/templates/nova_network.pp
@@ -3,6 +3,6 @@ class {"nova::network":
     enabled => true,
     private_interface => '%(CONFIG_NOVANETWORK_PRIVIF)s',
     public_interface => '%(CONFIG_NOVANETWORK_PUBIF)s',
-    fixed_range => '192.168.11.0/24',
-    floating_range => '192.168.13.0/24'
+    fixed_range => '%(CONFIG_NOVANETWORK_FIXEDRANGE)s',
+    floating_range => '%(CONFIG_NOVANETWORK_FLOATINGRANGE)s'
 }
