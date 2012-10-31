677b8eeeab4da605e143892b03c01bf2258fe4d5
Derek Higgins <derekh@redhat.com>
2012-10-31 09:26:23 -0400

    Removing notifies
    These notifies arn't valid with the new modules and can be removed,
    I may need alternative notifies but I havn't hit any problems yet

--- puppet/templates/nova_api.pp
+++ puppet/templates/nova_api.pp
@@ -6,12 +6,6 @@ class {"nova::api":
     admin_password => "nova_default_password",
 }
 
-# the notify in the nova:api class doesn't work, so need to set these ones
-# TODO : figure this out
-Package<| title == 'nova-common' |> ~> Exec['initial-db-sync']
-Package<| title == 'nova-common' |> ~> File['/etc/nova/api-paste.ini']
-
-
 firewall { '001 novaapi incomming':
     proto    => 'tcp',
     dport    => ['8773', '8774', '8776'],
