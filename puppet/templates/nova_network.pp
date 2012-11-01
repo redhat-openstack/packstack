30b50871f08f2b08817c92ef62e80d7b0d7efc6f
Derek Higgins <derekh@redhat.com>
2012-11-01 11:59:13 -0400

    Explicitly set force_dhcp_release

--- puppet/templates/nova_network.pp
+++ puppet/templates/nova_network.pp
@@ -4,5 +4,6 @@ class {"nova::network":
     private_interface => '%(CONFIG_NOVA_NETWORK_PRIVIF)s',
     public_interface => '%(CONFIG_NOVA_NETWORK_PUBIF)s',
     fixed_range => '%(CONFIG_NOVA_NETWORK_FIXEDRANGE)s',
-    floating_range => '%(CONFIG_NOVA_NETWORK_FLOATINGRANGE)s'
+    floating_range => '%(CONFIG_NOVA_NETWORK_FLOATINGRANGE)s',
+    config_overrides => {force_dhcp_release => false}
 }
