986894daf000dab1cdc623d0e76ee738689c5ab5
Derek Higgins <derekh@redhat.com>
2012-12-11 07:17:11 -0500

    Adding support to configure yum repositories

--- packstack/plugins/serverprep_901.py
+++ packstack/plugins/serverprep_901.py
@@ -36,6 +36,42 @@ def initConfig(controllerObject):
                    "USE_DEFAULT"     : False,
                    "NEED_CONFIRM"    : False,
                    "CONDITION"       : False },
+                  {"CMD_OPTION"      : "additional-repo",
+                   "USAGE"           : "A comma seperated list of urls to any additional repositories to install",
+                   "PROMPT"          : "A comma seperated list of urls to any additional repositories to install",
+                   "OPTION_LIST"     : [],
+                   "VALIDATION_FUNC" : lambda a,b: True,
+                   "DEFAULT_VALUE"   : "",
+                   "MASK_INPUT"      : False,
+                   "LOOSE_VALIDATION": True,
+                   "CONF_NAME"       : "CONFIG_REPO",
+                   "USE_DEFAULT"     : False,
+                   "NEED_CONFIRM"    : False,
+                   "CONDITION"       : False },
+                  {"CMD_OPTION"      : "rh-username",
+                   "USAGE"           : "To subscribe each server to Red Hat, include this with CONFIG_RH_PASSWORD",
+                   "PROMPT"          : "To subscribe each server to Red Hat, include this with CONFIG_RH_PASSWORD",
+                   "OPTION_LIST"     : [],
+                   "VALIDATION_FUNC" : lambda a,b: True,
+                   "DEFAULT_VALUE"   : "",
+                   "MASK_INPUT"      : False,
+                   "LOOSE_VALIDATION": True,
+                   "CONF_NAME"       : "CONFIG_RH_USERNAME",
+                   "USE_DEFAULT"     : False,
+                   "NEED_CONFIRM"    : False,
+                   "CONDITION"       : False },
+                  {"CMD_OPTION"      : "rh-password",
+                   "USAGE"           : "To subscribe each server to Red Hat, include this with CONFIG_RH_USERNAME",
+                   "PROMPT"          : "To subscribe each server to Red Hat, include this with CONFIG_RH_USERNAME",
+                   "OPTION_LIST"     : [],
+                   "VALIDATION_FUNC" : lambda a,b: True,
+                   "DEFAULT_VALUE"   : "",
+                   "MASK_INPUT"      : False,
+                   "LOOSE_VALIDATION": True,
+                   "CONF_NAME"       : "CONFIG_RH_PASSWORD",
+                   "USE_DEFAULT"     : False,
+                   "NEED_CONFIRM"    : False,
+                   "CONDITION"       : False },
             ]
         }
 
@@ -67,7 +103,24 @@ def serverprep():
 
         # install epel if on rhel and epel is configured
         if controller.CONF["CONFIG_USE_EPEL"] == 'y':
-            server.append("grep 'Red Hat Enterprise Linux' /etc/redhat-release && ( rpm -q epel-release || rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm ) || echo -n ''")
+            server.append("grep 'Red Hat Enterprise Linux' /etc/redhat-release && ( rpm -q epel-release || \
+                           rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm ) || echo -n ''")
         server.append("mkdir -p %s"%basedefs.PUPPET_MANIFEST_DIR)
-        server.execute()
 
+        # Add yum repositories if configured
+        CONFIG_REPO = controller.CONF["CONFIG_REPO"].strip()
+        if CONFIG_REPO:
+            for i, url in enumerate(CONFIG_REPO.split(',')):
+                reponame = 'packstack_%d'%i
+                server.append('echo "[%s]\nname=%s\nbaseurl=%s\nenabled=1\ngpgcheck=0" > /etc/yum.repos.d/%s.repo'%(reponame, reponame, url, reponame))
+
+        # Subscribe to Red Hat Repositories if configured
+        RH_USERNAME = controller.CONF["CONFIG_RH_USERNAME"].strip()
+        if RH_USERNAME:
+            server.append("subscription-manager register --username=%s --password=%s --autosubscribe || true"%(RH_USERNAME, controller.CONF["CONFIG_RH_PASSWORD"].strip()))
+            server.append("subscription-manager list --consumed | grep -i openstack || "
+                          "subscription-manager subscribe --pool $(subscription-manager list --available | grep -e 'Red Hat OpenStack' -m 1 -A 2 | grep 'Pool Id' | awk '{print $3}')")
+            server.append("yum clean all")
+            server.append("yum-config-manager --enable rhel-server-ost-6-folsom-rpms")
+
+        server.execute()
