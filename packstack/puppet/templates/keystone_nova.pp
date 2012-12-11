81b1958324983e5c57bc7e4ce6bf7e6be86cce72
Derek Higgins <derekh@redhat.com>
2012-12-11 10:41:03 -0500

    Prevent the nova-volumes endpoint from being created
    It was being created along with the cinder endpoint. Probably
    causing cinder calls to fail in 50% of installs where cinder and
    nova-api are on different servers

--- packstack/puppet/templates/keystone_nova.pp
+++ packstack/puppet/templates/keystone_nova.pp
@@ -4,5 +4,6 @@ class {"nova::keystone::auth":
     public_address => "%(CONFIG_NOVA_API_HOST)s",
     admin_address => "%(CONFIG_NOVA_API_HOST)s",
     internal_address => "%(CONFIG_NOVA_API_HOST)s",
+    cinder => true,
 }
 
