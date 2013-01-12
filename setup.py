607acaf85757cc3f79170c3f326836b1c858cb7b
Derek Higgins <derekh@redhat.com>
2013-01-12 00:21:07 +0000

    Creating a single unit and pep8 test
    Starting with one of each to make sure the unit and pep8 gate tests work
    
    Change-Id: Ia4e5595df8937c8612143e19e21e17cfdc18a4b1

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 from packstack import version
 
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -11,14 +12,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "packstack",
-    version = version.version_string(),
-    author = "Derek Higgins",
-    author_email = "derekh@redhat.com",
-    description = ("A utility to install openstack"),
-    license = "ASL 2.0",
-    keywords = "openstack",
-    url = "https://github.com/fedora-openstack/packstack",
+    name="packstack",
+    version=version.version_string(),
+    author="Derek Higgins",
+    author_email="derekh@redhat.com",
+    description=("A utility to install openstack"),
+    license="ASL 2.0",
+    keywords="openstack",
+    url="https://github.com/fedora-openstack/packstack",
     packages=find_packages('.'),
     include_package_data=True,
     long_description=read('README'),
@@ -30,4 +31,3 @@ setup(
     ],
     scripts=["bin/packstack"]
 )
-
