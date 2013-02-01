f6dbc9f5cc061ec1ecfab1a921f309087d8afa53
Flaper Fesp <flaper87@gmail.com>
2013-02-01 10:10:50 +0100

    Create a cinder volume if it doesn't exists
    This patch adds support for volume group creation. Since we're not able
    to change cinder's volume name in cinder's confgs (yet), we'll be
    prompting the user just for the volume's path and size
    
    Change-Id: Ie6e65a67c0a926afe6ad25c5d12bb9b265a2408b

--- packstack/installer/exceptions.py
+++ packstack/installer/exceptions.py
@@ -5,12 +5,16 @@ __all__ = (
 
     'InstallError',
     'FlagValidationError',
+    'MissingRequirements',
 
     'PluginError',
     'ParamProcessingError',
+    'ParamValidationError',
 
     'NetworkError',
     'ScriptRuntimeError',
+
+     
 )
 
 
@@ -18,20 +22,30 @@ class PackStackError(Exception):
     """Default Exception class for packstack installer."""
     pass
 
+
+class MissingRequirements(PackStackError):
+    """Raised when minimum install requirements are not met"""
+    pass
+
+
 class InstallError(PackStackError):
     """Exception for generic errors during setup run."""
     pass
+
 
 class FlagValidationError(InstallError):
     """Raised when single flag validation fails."""
     pass
 
+
 class ParamValidationError(InstallError):
     """Raised when parameter value validation fails."""
     pass
 
+
 class PluginError(PackStackError):
     pass
+
 
 class ParamProcessingError(PluginError):
     pass
@@ -40,6 +54,8 @@ class ParamProcessingError(PluginError):
 class NetworkError(PackStackError):
     """Should be used for packstack's network failures."""
     pass
+
+
 class ScriptRuntimeError(PackStackError):
     """Raised when ScriptRunner.execute does not end successfully."""
     pass
