327e7a24cd4b0e3c190ea1318034c98a963cc888
Sandro Mathys <sm@sandro-mathys.ch>
2013-02-05 21:45:14 +0100

    Remove pointless Debian support to simplify code
    Packstack is incompatible to Debian in many places, therefore
    supporting it in just one single place just makes no sense. If
    at all, complete instead of very partial Debian support should
    be introduced. Until such time it's easiest to support all the
    different Fedora and RHEL derivatives by having no operating
    system discriminator at all.
    
    Partially discussed at:
    https://bugzilla.redhat.com/show_bug.cgi?id=906269
    
    Change-Id: I93cd4d25b0f85bae24c97be6ed91547dce1935d7

--- packstack/puppet/templates/postscript.pp
+++ packstack/puppet/templates/postscript.pp
@@ -1,6 +1,3 @@
 exec { 'persist-firewall':
-  command     => $operatingsystem ? {
-    'debian'                  => '/sbin/iptables-save > /etc/iptables/rules.v4',
-    /(Fedora|RedHat|CentOS|Scientific)/  => '/sbin/iptables-save > /etc/sysconfig/iptables',
-  },
+  command => '/sbin/iptables-save > /etc/sysconfig/iptables',
 }
