8588df5b19d57d42cc04ba1390874fc29fdf4abb
Derek Higgins <derekh@redhat.com>
2013-02-21 08:07:12 -0500

    Tune the host with a virtual hosts profile
    https://bugzilla.redhat.com/show_bug.cgi?id=865347
    
    Change-Id: Ib5924bb4f603f33020eb7ee1136504df605702e9

--- packstack/puppet/templates/nova_compute.pp
+++ packstack/puppet/templates/nova_compute.pp
@@ -50,3 +50,20 @@ firewall { '001 nove compute incoming':
     dport    => '5900-5999',
     action   => 'accept',
 }
+
+
+# Tune the host with a virtual hosts profile
+package {'tuned':
+    ensure => present,
+}
+
+service {'tuned':
+    ensure => running,
+    require => Package['tuned'],
+}
+
+exec {'tuned-virtual-host':
+    unless => '/usr/sbin/tuned-adm active | /bin/grep virtual-host',
+    command => '/usr/sbin/tuned-adm profile virtual-host',
+    require => Service['tuned'],
+}
