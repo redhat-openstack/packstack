46f82c92bf17731fa490e09ceb9dbdc67980a25c
Derek Higgins <derekh@redhat.com>
2013-03-04 05:45:54 -0500

    Start the swift support services
    openstack-swift-account openstack-swift-account-auditor
    openstack-swift-account-reaper openstack-swift-account-replicator
    openstack-swift-container openstack-swift-container-auditor
    openstack-swift-container-replicator openstack-swift-container-updater
    openstack-swift-object openstack-swift-object-auditor
    openstack-swift-object-replicator openstack-swift-object-updater
    
    o Add dependency so that services arn't started before config
      files are in place
    
    Change-Id: Iafca5c717f7a6f1aac1de9da21ef68b55825f139

--- packstack/puppet/templates/swift_common.pp
+++ packstack/puppet/templates/swift_common.pp
@@ -1,6 +1,7 @@
 
 class { 'ssh::server::install': }
 
+Class['swift'] -> Service <| |>
 class { 'swift':
     # not sure how I want to deal with this shared secret
     swift_hash_suffix => 'swift_shared_secret',
