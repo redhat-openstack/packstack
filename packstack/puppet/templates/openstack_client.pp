e030f9ffc1df3d8bd9f5c34de84416e1f328f8ad
Derek Higgins <derekh@redhat.com>
2013-03-19 08:02:00 -0400

    Properly escape $ in PS1
    \$ has special menaing in PS1,
    \$ = a #, if the effective UID is 0, otherwise a $
    https://bugzilla.redhat.com/show_bug.cgi?id=920291
    
    Change-Id: I11cc78f89532dc3fc169d9853cea4547bf2ac382

--- packstack/puppet/templates/openstack_client.pp
+++ packstack/puppet/templates/openstack_client.pp
@@ -14,6 +14,6 @@ file {"${::home_dir}/keystonerc_admin":
 export OS_TENANT_NAME=admin
 export OS_PASSWORD=%(CONFIG_KEYSTONE_ADMIN_PW)s
 export OS_AUTH_URL=http://%(CONFIG_KEYSTONE_HOST)s:35357/v2.0/
-export PS1=\"[\\u@\\h \\W(keystone_admin)]\$ \"
+export PS1='[\\u@\\h \\W(keystone_admin)]\\$ '
 ",
 }
