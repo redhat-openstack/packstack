eaba97fb25d057f9bb7b6f75eda2e4d97088ee47
Martin Magr <mmagr@redhat.com>
2013-06-05 11:07:32 +0200

    Added submodule update to release.sh
    Change-Id: I50314153673bb1e061c80e369214ed25ac3bd5f1

--- bin/release.sh
+++ bin/release.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-# 
+#
+
+git reset --hard
+git submodule update --init
+git status -s | grep "." && ( echo "Contains unknown files" ; exit 1 )
+
 if [ "$1" = "release" ] ; then
     sed -i -e 's/FINAL=False/FINAL=True/g' packstack/version.py
     SNAPTAG=""
