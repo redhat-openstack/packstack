17636ddabe5ea54448960eec099b831dc745350d
Terry Wilson <twilson@redhat.com>
2013-07-03 10:46:05 -0500

    Add support for provider networks
    Allow setting CONFIG_QUANTUM_L3_EXT_BRIDGE=provider to use a provider
    network instead of creating the br-ex bridge. Also, add a new config
    option, CONFIG_QUANTUM_OVS_BRIDGE_IFACES, to allow adding network
    interfaces to OVS bridges. For example:
    
    CONFIG_QUANTUM_OVS_VLAN_RANGES=physnet1:1000:2000
    CONFIG_QUANTUM_OVS_BRIDGE_MAPPINGS=physnet1:br-eth1
    CONFIG_QUANTUM_OVS_BRIDGE_IFACES=br-eth1:eth1
    
    would set up a network labeled 'physnet1' with an OVS bridge named
    br-eth1, and add the eth1 interface to the br-eth1 OVS bridge.
    
    Change-Id: I9c9f4c22b15ab740dc09ede2d33c9499c0105732

--- packstack/puppet/templates/quantum_l3.pp
+++ packstack/puppet/templates/quantum_l3.pp
@@ -1,5 +1,5 @@
 class { 'quantum::agents::l3':
-  use_namespaces => '%(CONFIG_QUANTUM_USE_NAMESPACES)s',
-  interface_driver => '%(CONFIG_QUANTUM_L3_INTERFACE_DRIVER)s',
-  external_network_bridge => '%(CONFIG_QUANTUM_L3_EXT_BRIDGE)s',
+  use_namespaces          => '%(CONFIG_QUANTUM_USE_NAMESPACES)s',
+  interface_driver        => '%(CONFIG_QUANTUM_L3_INTERFACE_DRIVER)s',
+  external_network_bridge => %(CONFIG_QUANTUM_L3_EXT_BRIDGE_STR)s,
 }
