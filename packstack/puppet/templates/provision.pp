d5378990ad92f9a80e812ba2b2dceff43e26f374
Terry Wilson <twilson@redhat.com>
2013-07-15 13:58:52 -0500

    Add firewall rules for demo outbound connectivity
    Change-Id: I9714247409bb5c106731a393282be45eb72ee239

--- packstack/puppet/templates/provision.pp
+++ packstack/puppet/templates/provision.pp
@@ -1,5 +1,28 @@
 class { 'openstack::provision':
   admin_password      => '%(CONFIG_KEYSTONE_ADMIN_PW)s',
   configure_tempest   => %(CONFIG_PROVISION_TEMPEST)s,
-  setup_ovs_bridge    => %(CONFIG_PROVISION_ALL_IN_ONE_OVS_BRIDGE)s
+  setup_ovs_bridge    => %(CONFIG_PROVISION_ALL_IN_ONE_OVS_BRIDGE)s,
+  public_bridge_name  => '%(CONFIG_QUANTUM_L3_EXT_BRIDGE)s'
+}
+
+firewall { '000 nat':
+  chain  => 'POSTROUTING',
+  jump   => 'MASQUERADE',
+  source => $::openstack::provision::floating_range,
+  table => 'nat',
+  proto => 'all',
+}
+
+firewall { '000 forward out':
+  chain => 'FORWARD',
+  action  => 'accept',
+  outiface => '%(CONFIG_QUANTUM_L3_EXT_BRIDGE)s',
+  proto => 'all',
+}
+
+firewall { '000 forward in':
+  chain => 'FORWARD',
+  action  => 'accept',
+  iniface => '%(CONFIG_QUANTUM_L3_EXT_BRIDGE)s',
+  proto => 'all',
 }
