19817bd54c6827acd2ff5f573c72a325a08ee6b2
Leif Madsen <leif@leifmadsen.com>
2013-07-17 09:39:27 -0400

    Update documentation grammar.
    Change-Id: I042863c14b7d842575a49510b66b1af2e3ec7a20

--- packstack/puppet/templates/prescript.pp
+++ packstack/puppet/templates/prescript.pp
@@ -1,5 +1,5 @@
 
-$warning = "Kernel package with netns support has been installed on host $::ipaddress. Please note that with this action you are loosing Red Hat support for this host. Because of the kernel update host mentioned above requires reboot."
+$warning = "Kernel package with netns support has been installed on host $::ipaddress. Please note that with this action you are losing Red Hat support for this host. Because of the kernel update the host mentioned above requires reboot."
 
 class { 'packstack::netns':
     warning => $warning
