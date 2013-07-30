18d60080f3bf85766c57d9a1d52ac3179859de09
Martin Magr <mmagr@redhat.com>
2013-07-30 09:51:23 +0200

    Inform about support only on RHEL
    Change-Id: I66fabfd64284295e1417809948db57eef1b713f0
    Fixes: rhbz#975913

--- packstack/puppet/templates/prescript.pp
+++ packstack/puppet/templates/prescript.pp
@@ -1,5 +1,9 @@
 
-$warning = "Kernel package with netns support has been installed on host $::ipaddress. Please note that with this action you are losing Red Hat support for this host. Because of the kernel update the host mentioned above requires reboot."
+if $::operatingsystem == 'RedHat' {
+    $warning = "Kernel package with netns support has been installed on host $::ipaddress. Please note that with this action you are losing Red Hat support for this host. Because of the kernel update the host mentioned above requires reboot."
+} else {
+    $warning = "Kernel package with netns support has been installed on host $::ipaddress. Because of the kernel update the host mentioned above requires reboot."
+}
 
 class { 'packstack::netns':
     warning => $warning
