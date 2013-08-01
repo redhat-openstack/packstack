0218ca12d42f00a39cdeb3583d0a19203c63f6f7
Maru Newby <marun@redhat.com>
2013-07-31 21:51:10 -0700

    Allow tempest repo uri and revision configuration
     * Different revisions of tempest are required for different
       releases of openstack, so it is helpful to be able to choose
       the git repo and repo revision that will be deployed.
     * The vcsrepo submodule is updated so that remote revisions are
       supported - previously providing a git revision did not work.
    
    Change-Id: If89c06d7c909dd4e5cc7b5b07291bf526a22ea70

--- packstack/puppet/templates/provision.pp
+++ packstack/puppet/templates/provision.pp
@@ -1,9 +1,11 @@
 class { 'openstack::provision':
-  admin_password      => '%(CONFIG_KEYSTONE_ADMIN_PW)s',
-  password            => '%(CONFIG_KEYSTONE_DEMO_PW)s',
-  configure_tempest   => %(CONFIG_PROVISION_TEMPEST)s,
-  setup_ovs_bridge    => %(CONFIG_PROVISION_ALL_IN_ONE_OVS_BRIDGE)s,
-  public_bridge_name  => '%(CONFIG_NEUTRON_L3_EXT_BRIDGE)s'
+  admin_password        => '%(CONFIG_KEYSTONE_ADMIN_PW)s',
+  password              => '%(CONFIG_KEYSTONE_DEMO_PW)s',
+  configure_tempest     => %(CONFIG_PROVISION_TEMPEST)s,
+  tempest_repo_uri      => '%(CONFIG_PROVISION_TEMPEST_REPO_URI)s',
+  tempest_repo_revision => '%(CONFIG_PROVISION_TEMPEST_REPO_REVISION)s',
+  setup_ovs_bridge      => %(CONFIG_PROVISION_ALL_IN_ONE_OVS_BRIDGE)s,
+  public_bridge_name    => '%(CONFIG_NEUTRON_L3_EXT_BRIDGE)s'
 }
 
 firewall { '000 nat':
