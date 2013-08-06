e244d5fbe80c4efc984c52455522b0c4565a615a
Terry Wilson <twilson@redhat.com>
2013-08-06 04:10:30 -0500

    Make demo floating_range configurable
    Change-Id: I5277cd66480c0c7743af326c9baf16278dc00fe8
    Resolves: rhbz#999240

--- packstack/puppet/templates/provision.pp
+++ packstack/puppet/templates/provision.pp
@@ -5,28 +5,31 @@ class { 'openstack::provision':
   tempest_repo_uri      => '%(CONFIG_PROVISION_TEMPEST_REPO_URI)s',
   tempest_repo_revision => '%(CONFIG_PROVISION_TEMPEST_REPO_REVISION)s',
   setup_ovs_bridge      => %(CONFIG_PROVISION_ALL_IN_ONE_OVS_BRIDGE)s,
-  public_bridge_name    => '%(CONFIG_NEUTRON_L3_EXT_BRIDGE)s'
+  public_bridge_name    => '%(CONFIG_NEUTRON_L3_EXT_BRIDGE)s',
+  floating_range        => '%(CONFIG_PROVISION_DEMO_FLOATRANGE)s',
 }
 
-firewall { '000 nat':
-  chain  => 'POSTROUTING',
-  jump   => 'MASQUERADE',
-  source => $::openstack::provision::floating_range,
-  outiface => $::gateway_device,
-  table => 'nat',
-  proto => 'all',
-}
+if %(CONFIG_PROVISION_ALL_IN_ONE_OVS_BRIDGE)s {
+  firewall { '000 nat':
+    chain  => 'POSTROUTING',
+    jump   => 'MASQUERADE',
+    source => $::openstack::provision::floating_range,
+    outiface => $::gateway_device,
+    table => 'nat',
+    proto => 'all',
+  }
 
-firewall { '000 forward out':
-  chain => 'FORWARD',
-  action  => 'accept',
-  outiface => '%(CONFIG_NEUTRON_L3_EXT_BRIDGE)s',
-  proto => 'all',
-}
+  firewall { '000 forward out':
+    chain => 'FORWARD',
+    action  => 'accept',
+    outiface => '%(CONFIG_NEUTRON_L3_EXT_BRIDGE)s',
+    proto => 'all',
+  }
 
-firewall { '000 forward in':
-  chain => 'FORWARD',
-  action  => 'accept',
-  iniface => '%(CONFIG_NEUTRON_L3_EXT_BRIDGE)s',
-  proto => 'all',
+  firewall { '000 forward in':
+    chain => 'FORWARD',
+    action  => 'accept',
+    iniface => '%(CONFIG_NEUTRON_L3_EXT_BRIDGE)s',
+    proto => 'all',
+  }
 }
