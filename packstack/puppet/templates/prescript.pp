5e96348574c34454b8fe57305190d7463c506c22
Martin Magr <mmagr@redhat.com>
2013-08-30 11:07:45 +0200

    Install openstack-selinux on all EL distros
    Change-Id: Ie965b1504381f2c0d1cb127ff586fd395dade053
    Fixes: rhbz#996773

--- packstack/puppet/templates/prescript.pp
+++ packstack/puppet/templates/prescript.pp
@@ -1,4 +1,10 @@
 
+if $::operatingsystem != "Fedora" {
+    package{ 'openstack-selinux':
+        ensure => present,
+    }
+}
+
 if $::operatingsystem == 'RedHat' {
     $warning = "Kernel package with netns support has been installed on host $::ipaddress. Please note that with this action you are losing Red Hat support for this host. Because of the kernel update the host mentioned above requires reboot."
 } else {
