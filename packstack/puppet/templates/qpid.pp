d2275e73b408bb9b1e8cd422cb0da478c25e2417
Martin Magr <mmagr@redhat.com>
2013-09-09 11:29:46 +0200

    Revert "Added SSL option to the qpid plugin"
    This reverts commit d7591ed124a8ee6bc82dff150b8cc1297bfabf43.
    
    Change-Id: Ie28659850c46664d626e52219b1688542fa998dd

--- packstack/puppet/templates/qpid.pp
+++ packstack/puppet/templates/qpid.pp
@@ -2,14 +2,8 @@
 class {"qpid::server":
     auth => "no",
     clustered => false,
-    ssl_certdb_pw      => '%(CONFIG_QPID_NSS_CERTDB_PW)s',
-    ssl_cert_name      => '%(CONFIG_QPID_NSS_CERT_NAME)s',
-    ssl_port           => '%(CONFIG_QPID_SSL_PORT)s',
-    ssl                => %(CONFIG_QPID_ENABLE_SSL)s,
-    ssl_cert          => '%(CONFIG_QPID_SSL_CERT_FILE)s',
-    ssl_key           => '%(CONFIG_QPID_SSL_KEY_FILE)s'
 }
-   
+
 firewall { '001 qpid incoming':
     proto    => 'tcp',
     dport    => ['5672'],
