b99766de525a930e1f0a7d068898238bb494bc33
Ivan Chavero <ichavero@redhat.com>
2013-11-01 11:38:09 -0700

    Fixed missing host_iter function in grizzly
    Some functions that were added in havana were
    missing from grizzly and caused conflicts.
    
    Change-Id: I95679cffc7d79c48b4c2e77fd1d73e6a330af55d
    Fixes: rhbz#1021527

---
+++ packstack/installer/utils/shortcuts.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import grp
+import os
+import pwd
+
+
+def host_iter(config):
+    for key, value in config.iteritems():
+        if key.endswith("_HOST"):
+            host = value.split('/')[0]
+            yield key, host
+        if key.endswith("_HOSTS"):
+            for i in value.split(","):
+                host = i.strip().split('/')[0]
+                yield key, host
+
+
+def hosts(config):
+    result = set()
+    for key, host in host_iter(config):
+        result.add(host)
+    return result
+
+
+def get_current_user():
+    try:
+        user = pwd.getpwnam(os.getlogin())
+        uid, gid = user.pw_uid, user.pw_gid
+    except OSError:
+        # in case program is run by a script
+        uid, gid = os.getuid(), os.getgid()
+    return uid, gid
+
+
+def get_current_username():
+    uid, gid = get_current_user()
+    user = pwd.getpwuid(uid).pw_name
+    group = grp.getgrgid(gid).gr_name
+    return user, group
