7be825d7b3fcd5be526ff9920516492c3c1a0e6e
Martin Magr <mmagr@redhat.com>
2013-11-20 17:29:27 +0100

    ML2 support
    Divided CONFIG_NEUTRON_L2_PLUGIN parameter to CONFIG_NEUTRON_L2_PLUGIN
    and CONFIG_NEUTRON_L2_AGENT to enable separate installation of agent
    in case of ML2 plugin installation.
    
    Implemented ML2 plugin installation using classes from puppet-neutron
    module.
    
    Fixes: rhbz#1017144
    
    Change-Id: I6b769b7aec894f2ef87d9b33dd02fbc20abb9e3a

---
+++ packstack/puppet/templates/neutron_ml2_plugin.pp
@@ -0,0 +1,19 @@
+
+# This is should be implemented in plugin class, will submit patch upstream later
+package {'openstack-neutron-ml2':
+  ensure => 'installed',
+  before => Class['neutron::plugins::ml2']
+}
+
+class { 'neutron::plugins::ml2':
+  type_drivers         => %(CONFIG_NEUTRON_ML2_TYPE_DRIVERS)s,
+  tenant_network_types => %(CONFIG_NEUTRON_ML2_TENANT_NETWORK_TYPES)s,
+  mechanism_drivers    => %(CONFIG_NEUTRON_ML2_MECHANISM_DRIVERS)s,
+  flat_networks        => %(CONFIG_NEUTRON_ML2_FLAT_NETWORKS)s,
+  network_vlan_ranges  => %(CONFIG_NEUTRON_ML2_VLAN_RANGES)s,
+  tunnel_id_ranges     => %(CONFIG_NEUTRON_ML2_TUNNEL_ID_RANGES)s,
+  vxlan_group          => %(CONFIG_NEUTRON_ML2_VXLAN_GROUP)s,
+  vni_ranges           => %(CONFIG_NEUTRON_ML2_VNI_RANGES)s,
+}
+
+Neutron_plugin_ml2<||> -> File['/etc/neutron/plugin.ini'] -> Exec['neutron-db-manage upgrade']
