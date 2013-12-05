1ed09b51df6244178157fab8ebbce7bf7720b445
Francesco Vollero <fvollero@redhat.com>
2013-12-05 11:38:41 +0100

    Added the help_url pointing to RH doc
    Fixes: rhbz#1030398
    
    Change-Id: I6b7637331a99059bea96f0a83e75c1807c784890

--- packstack/puppet/templates/horizon.pp
+++ packstack/puppet/templates/horizon.pp
@@ -15,6 +15,7 @@ class {'horizon':
    keystone_host => '%(CONFIG_KEYSTONE_HOST)s',
    fqdn => ['%(CONFIG_HORIZON_HOST)s', "$::fqdn"],
    can_set_mount_point => 'False',
+   help_url =>'https://access.redhat.com/site/documentation//en-US/Red_Hat_Enterprise_Linux_OpenStack_Platform/4/index.html'
 }
 
 class {'memcached':}
