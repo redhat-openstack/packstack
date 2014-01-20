db71879e94a3ca8daf4bdd744fe345abf7178d6f
Francesco Vollero <fvollero@redhat.com>
2014-01-20 17:01:20 +0100

    [Enhancement] Added --version flag
    Change-Id: I4f90673ce4855ada405c4e93c9f4c2c08707e6fe
    Fix: bz#1011691

--- packstack/version.py
+++ packstack/version.py
@@ -1,11 +1,14 @@
 
-VERSION = ['2013', '2', '1']
+VERSION = ['2014', '1', '1']
 FINAL=False
+RELEASE="Icehouse"
+SNAPTAG=None
+
+def release_string():
+    return RELEASE
 
 def version_string():
     if FINAL:
         return '.'.join(filter(None, VERSION))
     else:
-        return '.'.join(filter(None, VERSION))+"dev"
-        
-
+        return '.'.join(filter(None, VERSION))+"dev{0}".format(SNAPTAG)
