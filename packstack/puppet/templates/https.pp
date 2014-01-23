b0b2449a73c4ec3443fc3750f974b5e85571acce
Ivan Chavero <ichavero@redhat.com>
2014-01-22 22:11:22 -0700

    Fixes nagios overwrite of apache configuration
    The apache puppet module was on a version that deleted
    configurations by default, this patch upgrades to the
    latest version of the upstream module which has a feature
    that allows to keep the configurations; also modifies the
    nagios and horizon templates to use this feature,
    bringing a decoupling and clean up of some extra code
    from the templates.
    
    Change-Id: I4032d138a404111aed7368c7b8d2593df7ae16d1
    Fixes: rhbz#1054498

--- packstack/puppet/templates/https.pp
+++ packstack/puppet/templates/https.pp
@@ -29,37 +29,45 @@ file_line{'nohttp_ip':
     require =>  Class['horizon']
 }
 
-
 # if the mod_ssl apache puppet module does not install
 # this file, we ensure it haves the minimum
 # requirements for SSL to work
-file {'/etc/httpd/conf.d/ssl.conf':
-    path => '/etc/httpd/conf.d/ssl.conf',
-    ensure => file,
-    mode => '0644'
-} -> file_line{'ssl_port':
-    path  => '/etc/httpd/conf.d/ssl.conf',
-    match => 'Listen .+',
-    line  => 'Listen 443',
-    require =>  Class['apache::mod::ssl']
-} -> file_line{'ssl_engine':
-    path => '/etc/httpd/conf.d/ssl.conf',
-    match => 'SSLEngine .+',
-    line => 'SSLEngine on',
-    require =>  Class['apache::mod::ssl']
+$ssl_lines = {
+    'ssl_port' => {
+        path  => '/etc/httpd/conf.d/ssl.conf',
+        match => 'Listen .+',
+        line  => 'Listen 443',
+        require =>  Class['apache::mod::ssl']
+    },
+    'start_vhost_ssl' => {
+        path => '/etc/httpd/conf.d/ssl.conf',
+        line => '<VirtualHost *:443>',
+        require => File_line['ssl_port'],
+     },
+    'ssl_engine' => {
+        path => '/etc/httpd/conf.d/ssl.conf',
+        match => 'SSLEngine .+',
+        line => 'SSLEngine on',
+        require =>   File_line['start_vhost_ssl'],
+     },
+    # set the name of the ssl cert and key file
+    'sslcert' => {
+        path => '/etc/httpd/conf.d/ssl.conf',
+        match => '^SSLCertificateFile ',
+        line => 'SSLCertificateFile /etc/pki/tls/certs/ssl_ps_server.crt',
+        require => File_line['ssl_engine'],
+    },
+    'sslkey' => {
+        path => '/etc/httpd/conf.d/ssl.conf',
+        match => '^SSLCertificateKeyFile ',
+        line => 'SSLCertificateKeyFile /etc/pki/tls/private/ssl_ps_server.key',
+        require =>  File_line['sslcert'],
+    },
+    'end_vhost_ssl' => {
+        path => '/etc/httpd/conf.d/ssl.conf',
+        line => '</VirtualHost>',
+        require => File_line['sslkey'],
+     },
 }
 
-# set the name of the ssl cert and key file
-file_line{'sslcert':
-    path => '/etc/httpd/conf.d/ssl.conf',
-    match => '^SSLCertificateFile ',
-    line => 'SSLCertificateFile /etc/pki/tls/certs/ssl_ps_server.crt',
-    require =>  Class['apache::mod::ssl']
-}
-
-file_line{'sslkey':
-    path => '/etc/httpd/conf.d/ssl.conf',
-    match => '^SSLCertificateKeyFile ',
-    line => 'SSLCertificateKeyFile /etc/pki/tls/private/ssl_ps_server.key',
-    require =>  Class['apache::mod::ssl']
-}
+create_resources(file_line, $ssl_lines)
