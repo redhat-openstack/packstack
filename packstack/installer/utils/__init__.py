2273a0e711df896443168c85d7c2903bca5c974d
Martin Magr <mmagr@redhat.com>
2014-02-26 14:26:38 +0100

    Fixed status message alignment
    Change-Id: Ie279d7853f4e4db86dddcffdfaaf1d1f023098a4
    (cherry picked from commit 43bf761da2599fc6d447ac8be5bd57280986d31f)

--- packstack/installer/utils/__init__.py
+++ packstack/installer/utils/__init__.py
@@ -4,9 +4,10 @@ from .datastructures import SortedDict
 from .decorators import retry
 from .network import get_localhost_ip, host2ip, force_ip, device_from_ip
 from .shell import ScriptRunner, execute
-from .shortcuts import host_iter, hosts, get_current_user,\
-     get_current_username, split_hosts
-from .strings import COLORS, color_text, mask_string
+from .shortcuts import (host_iter, hosts, get_current_user,
+                        get_current_username, split_hosts)
+from .strings import (COLORS, color_text, mask_string, state_format,
+                      state_message)
 
 
 __all__ = ('SortedDict',
@@ -14,4 +15,5 @@ __all__ = ('SortedDict',
            'get_localhost_ip', 'host2ip', 'force_ip', 'device_from_ip',
            'ScriptRunner', 'execute',
            'host_iter', 'hosts', 'get_current_user', 'get_current_username',
-           'split_hosts', 'COLORS', 'color_text', 'mask_string')
+           'split_hosts', 'COLORS', 'color_text', 'mask_string',
+           'state_format', 'state_message')
