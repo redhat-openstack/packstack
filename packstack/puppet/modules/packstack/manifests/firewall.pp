cfcb7b64cc16fb8203371829d1ae66cb2a83b5d3
Martin Magr <mmagr@redhat.com>
2014-03-03 21:08:19 +0100

    Enable DHCP ports on neutron-dhcp-agent hosts
    Conflicts:
    	packstack/plugins/glance_200.py
    	packstack/plugins/qpid_002.py
    
    Change-Id: I7ed387d4ac9a397554b24c3ac318a42af7ed2901
    Fixes: rhbz#999695
    (cherry picked from commit 63d1b4811590112e8a4d672cbb39338a313d4c84)

---
+++ packstack/puppet/modules/packstack/manifests/firewall.pp
@@ -0,0 +1,23 @@
+# Create firewall rules to allow only the FIREWALL_ALLOWED
+# hosts that need to connect via FIREWALL_PORTS
+# using FIREWALL_CHAIN
+
+define packstack::firewall($host, $service_name, $chain = "INPUT", $ports) {
+  $source = $host ? {
+    'ALL' => '0.0.0.0/0',
+    default => $host,
+  }
+  $heading = $chain ? {
+    'OUTPUT' => 'outgoing',
+    default => 'incoming',
+  }
+
+  firewall { "001 ${service_name} ${heading} ${title}":
+    chain => $chain,
+    proto  => ['tcp', 'udp'],
+    dport  => $ports,
+    action => 'accept',
+    source => $source,
+  }
+}
+
