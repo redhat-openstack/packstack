a8d00288929de4d1d8a5cc4aa3fff483337f81a0
Francesco Vollero <fvollero@redhat.com>
2014-04-03 20:48:00 +0200

    [heat] updated the templates according to latest stable/havana
    Fixes: rhbz#1084216
    
    Change-Id: I075842a33cceada8690d318459aec1c8b483e54e

--- packstack/puppet/templates/keystone_heat.pp
+++ packstack/puppet/templates/keystone_heat.pp
@@ -1,20 +1,17 @@
+# heat::keystone::auth
+class {"heat::keystone::auth":
+    password => "%(CONFIG_HEAT_KS_PW)s",
+    public_address => "%(CONFIG_HEAT_HOST)s",
+    admin_address => "%(CONFIG_HEAT_HOST)s",
+    internal_address => "%(CONFIG_HEAT_HOST)s",
+}
 
 if '%(CONFIG_HEAT_CFN_INSTALL)s' == 'y' {
-    class {"heat::keystone::auth":
+    # heat::keystone::cfn
+    class {"heat::keystone::auth_cfn":
         password => "%(CONFIG_HEAT_KS_PW)s",
-        heat_public_address => "%(CONFIG_HEAT_HOST)s",
-        heat_admin_address => "%(CONFIG_HEAT_HOST)s",
-        heat_internal_address => "%(CONFIG_HEAT_HOST)s",
-        cfn_public_address => "%(CONFIG_HEAT_HOST)s",
-        cfn_admin_address => "%(CONFIG_HEAT_HOST)s",
-        cfn_internal_address => "%(CONFIG_HEAT_HOST)s",
-    }
-} else {
-    class {"heat::keystone::auth":
-        password => "%(CONFIG_HEAT_KS_PW)s",
-        heat_public_address => "%(CONFIG_HEAT_HOST)s",
-        heat_admin_address => "%(CONFIG_HEAT_HOST)s",
-        heat_internal_address => "%(CONFIG_HEAT_HOST)s",
-        cfn_auth_name => undef,
+        public_address => "%(CONFIG_HEAT_HOST)s",
+        admin_address => "%(CONFIG_HEAT_HOST)s",
+        internal_address => "%(CONFIG_HEAT_HOST)s",
     }
 }
