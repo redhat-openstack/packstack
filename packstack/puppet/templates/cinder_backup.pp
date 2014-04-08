ea405af3c34138498cd7a7de195ac5c28beca6f2
Gilles Dubreuil <gilles@redhat.com>
2014-04-08 12:24:24 +1000

    Added puppet resource sequence for cinder backup (used by swift)
      Fixes BZ#1075609
    
    Change-Id: I0b43bd956036370ebf2587e332924c7e7610d907
    (cherry picked from commit 475908e7e6d3358676936669b2131d8d6e176979)

--- packstack/puppet/templates/cinder_backup.pp
+++ packstack/puppet/templates/cinder_backup.pp
@@ -4,3 +4,5 @@ class {'cinder::backup':
 class {'cinder::backup::swift':
     backup_swift_url => 'http://%(CONFIG_SWIFT_PROXY)s:8080/v1/AUTH_'
 }
+
+Class['cinder::api'] ~> Service['cinder-backup']
