7488f1f671deb55a010efffa73e4d92176fab9ac
Martin Magr <mmagr@redhat.com>
2014-05-12 15:25:41 +0200

    Ensure sshkey title is unique
    When there are multiple ssh-keys for one host Nova installation fails
    with sshkey resource duplication. This patch fixing the issue.
    
    Change-Id: Ibe587a23b5c5ff3fd7e292aaa41f804687792b27

--- packstack/puppet/templates/sshkey.pp
+++ packstack/puppet/templates/sshkey.pp
@@ -1,7 +1,6 @@
-sshkey { '%(SSH_HOST_NAME)s':
+sshkey { '%(SSH_HOST_KEY_TYPE)s.%(SSH_HOST_NAME)s':
   ensure       => present,
   host_aliases => [%(SSH_HOST_ALIASES)s],
   key          => '%(SSH_HOST_KEY)s',
   type         => '%(SSH_HOST_KEY_TYPE)s',
 }
-
