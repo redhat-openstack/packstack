cd8f90d1380502e751d0a2e1f8e4dfaa62e1872a
Martin Magr <mmagr@redhat.com>
2014-05-13 16:18:56 +0200

    Removed unnecessary hacks
    In Havana release there was necessary to install plugin specific packages
    for ML2 because module had not implement it. This is not true for Icehouse
    release so hacks have to be removed.
    
    Removed also enable_security_group hack which breaks recent version
    of Neutron server.
    
    Change-Id: I35896166bfe4de2c9e49ba59579063fc0b8c666d
    Fixes: rhbz#1096510

--- packstack/puppet/templates/neutron_ml2_plugin.pp
+++ packstack/puppet/templates/neutron_ml2_plugin.pp
@@ -1,21 +1,4 @@
 
-# We need this before https://review.openstack.org/#/c/67004/ will be merged
-if 'openvswitch' in %(CONFIG_NEUTRON_ML2_MECHANISM_DRIVERS)s and !defined(Package['neutron-plugin-ovs']) {
-  package {'neutron-plugin-ovs':
-    name   => 'openstack-neutron-openvswitch',
-    ensure => 'installed',
-    before => Class['neutron::plugins::ml2']
-  }
-}
-
-if 'linuxbridge' in %(CONFIG_NEUTRON_ML2_MECHANISM_DRIVERS)s and !defined(Package['neutron-plugin-linuxbridge']) {
-  package {'neutron-plugin-linuxbridge':
-    name   => 'openstack-neutron-linuxbridge',
-    ensure => 'installed',
-    before => Class['neutron::plugins::ml2']
-  }
-}
-
 class { 'neutron::plugins::ml2':
   type_drivers          => %(CONFIG_NEUTRON_ML2_TYPE_DRIVERS)s,
   tenant_network_types  => %(CONFIG_NEUTRON_ML2_TENANT_NETWORK_TYPES)s,
@@ -25,7 +8,7 @@ class { 'neutron::plugins::ml2':
   tunnel_id_ranges      => %(CONFIG_NEUTRON_ML2_TUNNEL_ID_RANGES)s,
   vxlan_group           => %(CONFIG_NEUTRON_ML2_VXLAN_GROUP)s,
   vni_ranges            => %(CONFIG_NEUTRON_ML2_VNI_RANGES)s,
-  enable_security_group => 'dummy',
+  enable_security_group => true,
 }
 
 # For cases where "neutron-db-manage upgrade" command is called we need to fill config file first
