d666b3b7fcff9478541b8c5885981c8d49b5336e
Martin Magr <mmagr@redhat.com>
2014-06-05 17:23:49 +0200

    Add Keystone domain for Heat support
    This patch adds support for creation of Keystone domain for Heat
    and for proper configuration of Heat.
    
    For this patch to work [1] is required. This patch is not currently
    in upstream module yet, but it will be part of next o-p-m build.
    
    [1] https://github.com/paramite/puppet-heat/commit/3a38aa6eda5a57299f20bf0cabb10442e1a2cc89
    
    Change-Id: I6b9c0f112d2e85131d880f308b9f391b47891cd7
    Fixes: rhbz#1076172

--- packstack/puppet/templates/heat.pp
+++ packstack/puppet/templates/heat.pp
@@ -3,8 +3,18 @@ class { 'heat::api':
 }
 
 class { 'heat::engine':
-    heat_metadata_server_url      => 'http://%(CONFIG_CONTROLLER_HOST)s:8000',
-    heat_waitcondition_server_url => 'http://%(CONFIG_CONTROLLER_HOST)s:8000/v1/waitcondition',
-    heat_watch_server_url         => 'http://%(CONFIG_CONTROLLER_HOST)s:8003',
-    auth_encryption_key           => '%(CONFIG_HEAT_AUTH_ENC_KEY)s',
+  heat_metadata_server_url      => 'http://%(CONFIG_CONTROLLER_HOST)s:8000',
+  heat_waitcondition_server_url => 'http://%(CONFIG_CONTROLLER_HOST)s:8000/v1/waitcondition',
+  heat_watch_server_url         => 'http://%(CONFIG_CONTROLLER_HOST)s:8003',
+  auth_encryption_key           => '%(CONFIG_HEAT_AUTH_ENC_KEY)s',
+}
+
+class { 'heat::keystone::domain':
+  auth_url          => 'http://%(CONFIG_CONTROLLER_HOST)s:35357/v2.0',
+  keystone_admin    => 'admin',
+  keystone_password => '%(CONFIG_KEYSTONE_ADMIN_PW)s',
+  keystone_tenant   => 'admin',
+  domain_name       => '%(CONFIG_HEAT_DOMAIN)s',
+  domain_admin      => '%(CONFIG_HEAT_DOMAIN_ADMIN)s',
+  domain_password   => '%(CONFIG_HEAT_DOMAIN_PASSWORD)s',
 }
