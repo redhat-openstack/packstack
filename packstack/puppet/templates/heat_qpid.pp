b854a38f384049349c08339fdac56cf0f2b3d9ad
Martin Magr <mmagr@redhat.com>
2014-06-12 13:08:24 +0200

    Set EC2 auth url for Heat
    Leaving keystone_ec2_uri with default value is causing Keystone
    authentification errors for Heat. This patch specifies correct URL.
    
    Change-Id: I9b2a786eeee8efd62944e792ed9eb428e842f24e
    Fixes: rhbz#1106394

--- packstack/puppet/templates/heat_qpid.pp
+++ packstack/puppet/templates/heat_qpid.pp
@@ -2,6 +2,7 @@ class { 'heat':
     keystone_host     => '%(CONFIG_CONTROLLER_HOST)s',
     keystone_password => '%(CONFIG_HEAT_KS_PW)s',
     auth_uri          => 'http://%(CONFIG_CONTROLLER_HOST)s:35357/v2.0',
+    keystone_ec2_uri  => 'http://%(CONFIG_CONTROLLER_HOST)s:35357/v2.0',
     rpc_backend       => 'heat.openstack.common.rpc.impl_qpid',
     qpid_hostname     => '%(CONFIG_AMQP_HOST)s',
     qpid_username     => '%(CONFIG_AMQP_AUTH_USER)s',
