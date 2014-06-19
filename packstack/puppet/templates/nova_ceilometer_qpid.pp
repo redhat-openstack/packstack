0a9560ccd758462fc4db8a93487d8bcb3739c12e
Lukas Bezdicka <lbezdick@redhat.com>
2014-06-19 18:54:11 +0200

    Unite qpid configuration across services
    Change-Id: I19001d2a160d61e2633d073677a8b64090cceba6

--- packstack/puppet/templates/nova_ceilometer_qpid.pp
+++ packstack/puppet/templates/nova_ceilometer_qpid.pp
@@ -4,6 +4,8 @@ class { 'ceilometer':
     qpid_hostname   => '%(CONFIG_AMQP_HOST)s',
     qpid_username  => '%(CONFIG_AMQP_AUTH_USER)s',
     qpid_password  => '%(CONFIG_AMQP_AUTH_PASSWORD)s',
+    qpid_port       => '%(CONFIG_AMQP_CLIENTS_PORT)s',
+    qpid_protocol   => '%(CONFIG_AMQP_PROTOCOL)s',
     rpc_backend     => 'ceilometer.openstack.common.rpc.impl_qpid',
     verbose         => true,
     debug           => %(CONFIG_DEBUG_MODE)s
