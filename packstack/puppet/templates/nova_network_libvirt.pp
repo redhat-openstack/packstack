c0d21428086830a02a1b3847dcb208da24a898c7
Ivan Chavero <ichavero@redhat.com>
2014-07-03 08:14:10 -0600

    Fixes libvirtd restart
    When libvirtd is restarted it kills it's socket
    wich causes nova to fail.
    
    Change-Id: I484ef56fe0b020cd457397b00aabac7da7a317bb
    Fixes: rhbz#1109362

--- packstack/puppet/templates/nova_network_libvirt.pp
+++ packstack/puppet/templates/nova_network_libvirt.pp
@@ -1,8 +1,8 @@
 $vmware_backend = '%(CONFIG_VMWARE_BACKEND)s'
 if $vmware_backend == 'n' {
-  exec { 'libvirtd_restart':
+  exec { 'libvirtd_reload':
     path => ['/usr/sbin/', '/sbin'],
-    command => 'service libvirtd restart',
+    command => 'service libvirtd reload',
     logoutput => 'on_failure',
     require => Class['nova::network'],
   }
