6f30cfc2c852d885a1f93d1ed0a66ae34e2e3b24
Gael Chamoulaud <gchamoul@redhat.com>
2014-07-10 18:06:36 +0200

    Sets l2population to true on nodes that populate l2 agents
    Change-Id: Idcd1bd8566e78bf16ee6299906dcc1d5415efc13
    Fixes: rhbz#1118010
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>
    Depends-on: https://github.com/redhat-openstack/openstack-puppet-modules/pull/81
    (cherry picked from commit 9027b757fc60dc1c99ee72c0206662d39c77b0f7)

--- packstack/puppet/templates/neutron_ovs_agent_gre.pp
+++ packstack/puppet/templates/neutron_ovs_agent_gre.pp
@@ -5,17 +5,34 @@ if "%(CONFIG_NEUTRON_OVS_TUNNEL_IF)s" {
   $localip = '%(CONFIG_NEUTRON_OVS_HOST)s'
 }
 
-class { 'neutron::agents::ovs':
-  bridge_mappings => %(CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS)s,
-  enable_tunneling => true,
-  tunnel_types => ['gre'],
-  local_ip => $localip,
-}
+$neutron_l2_plugin = '%(CONFIG_NEUTRON_L2_PLUGIN)s'
 
-file { 'ovs_neutron_plugin.ini':
-    path  => '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini',
-    owner => 'root',
-    group => 'neutron',
-    before => Service['ovs-cleanup-service'],
+if $neutron_l2_plugin == 'ml2' {
+  if ('l2population' in %(CONFIG_NEUTRON_ML2_MECHANISM_DRIVERS)s) {
+    $l2population = true
+  } else {
+    $l2population = false
+  }
+  class { 'neutron::agents::ml2::ovs':
+    bridge_mappings  => %(CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS)s,
+    enable_tunneling => true,
+    tunnel_types     => ['gre'],
+    local_ip         => $localip,
+    l2_population    => $l2population,
+  }
+} else {
+  class { 'neutron::agents::ovs':
+    bridge_mappings  => %(CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS)s,
+    enable_tunneling => true,
+    tunnel_types     => ['gre'],
+    local_ip         => $localip,
+  }
+
+  file { 'ovs_neutron_plugin.ini':
+    path    => '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini',
+    owner   => 'root',
+    group   => 'neutron',
+    before  => Service['ovs-cleanup-service'],
     require => Package['neutron-plugin-ovs'],
+  }
 }
--- packstack/puppet/templates/neutron_ovs_agent_vxlan.pp
+++ packstack/puppet/templates/neutron_ovs_agent_vxlan.pp
@@ -6,18 +6,36 @@ if "%(CONFIG_NEUTRON_OVS_TUNNEL_IF)s" {
   $localip = '%(CONFIG_NEUTRON_OVS_HOST)s'
 }
 
-class { 'neutron::agents::ovs':
-  bridge_mappings => %(CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS)s,
-  enable_tunneling => true,
-  tunnel_types => ['vxlan'],
-  local_ip => $localip,
-  vxlan_udp_port => %(CONFIG_NEUTRON_OVS_VXLAN_UDP_PORT)s,
-}
+$neutron_l2_plugin = '%(CONFIG_NEUTRON_L2_PLUGIN)s'
 
-file { 'ovs_neutron_plugin.ini':
-    path  => '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini',
-    owner => 'root',
-    group => 'neutron',
-    before => Service['ovs-cleanup-service'],
+if $neutron_l2_plugin == 'ml2' {
+  if ('l2population' in %(CONFIG_NEUTRON_ML2_MECHANISM_DRIVERS)s) {
+    $l2population = true
+  } else {
+    $l2population = false
+  }
+  class { 'neutron::agents::ml2::ovs':
+    bridge_mappings  => %(CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS)s,
+    enable_tunneling => true,
+    tunnel_types     => ['vxlan'],
+    local_ip         => $localip,
+    vxlan_udp_port   => %(CONFIG_NEUTRON_OVS_VXLAN_UDP_PORT)s,
+    l2_population    => $l2population,
+  }
+} else {
+  class { 'neutron::agents::ovs':
+    bridge_mappings  => %(CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS)s,
+    enable_tunneling => true,
+    tunnel_types     => ['vxlan'],
+    local_ip         => $localip,
+    vxlan_udp_port   => %(CONFIG_NEUTRON_OVS_VXLAN_UDP_PORT)s,
+  }
+
+  file { 'ovs_neutron_plugin.ini':
+    path    => '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini',
+    owner   => 'root',
+    group   => 'neutron',
+    before  => Service['ovs-cleanup-service'],
     require => Package['neutron-plugin-ovs'],
+  }
 }
