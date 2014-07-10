9027b757fc60dc1c99ee72c0206662d39c77b0f7
Gael Chamoulaud <gchamoul@redhat.com>
2014-07-10 18:06:36 +0200

    Sets l2population to true on nodes that populate l2 agents
    Change-Id: Idcd1bd8566e78bf16ee6299906dcc1d5415efc13
    Fixes: rhbz#1118010
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>
    Depends-on: https://github.com/redhat-openstack/openstack-puppet-modules/pull/81

--- packstack/puppet/templates/neutron_ovs_agent_local.pp
+++ packstack/puppet/templates/neutron_ovs_agent_local.pp
@@ -1,11 +1,25 @@
-class { 'neutron::agents::ovs':
-  bridge_mappings => %(CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS)s,
-}
+$neutron_l2_plugin = '%(CONFIG_NEUTRON_L2_PLUGIN)s'
 
-file { 'ovs_neutron_plugin.ini':
-    path  => '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini',
-    owner => 'root',
-    group => 'neutron',
-    before => Service['ovs-cleanup-service'],
+if $neutron_l2_plugin == 'ml2' {
+  if ('l2population' in %(CONFIG_NEUTRON_ML2_MECHANISM_DRIVERS)s) {
+    $l2population = true
+  } else {
+    $l2population = false
+  }
+  class { 'neutron::agents::ml2::ovs':
+    bridge_mappings => %(CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS)s,
+    l2_population   => $l2population,
+  }
+} else {
+  class { 'neutron::agents::ovs':
+    bridge_mappings => %(CONFIG_NEUTRON_OVS_BRIDGE_MAPPINGS)s,
+  }
+
+  file { 'ovs_neutron_plugin.ini':
+    path    => '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini',
+    owner   => 'root',
+    group   => 'neutron',
+    before  => Service['ovs-cleanup-service'],
     require => Package['neutron-plugin-ovs'],
+  }
 }
--- packstack/puppet/templates/neutron_ovs_bridge.pp
+++ packstack/puppet/templates/neutron_ovs_bridge.pp
@@ -1,4 +1,12 @@
+$neutron_l2_plugin = '%(CONFIG_NEUTRON_L2_PLUGIN)s'
+
+if $neutron_l2_plugin == 'ml2' {
+  $agent_service = 'neutron-ovs-agent-service'
+} else {
+  $agent_service = 'neutron-plugin-ovs-service'
+}
+
 vs_bridge { '%(CONFIG_NEUTRON_OVS_BRIDGE)s':
-  ensure => present,
-  require => Service['neutron-plugin-ovs-service']
+  ensure  => present,
+  require => Service["${agent_service}"]
 }
