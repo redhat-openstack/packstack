60aca0c5619396b05f98396436823901ab10ae38
Gael Chamoulaud <gchamoul@redhat.com>
2014-07-11 13:10:44 +0200

    Fixes Duplicated variables in neutron manifests
    Change-Id: Id30691dcc3c7340773bf0a6ea67acfbe94d949aa
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- packstack/puppet/templates/neutron_ovs_bridge.pp
+++ packstack/puppet/templates/neutron_ovs_bridge.pp
@@ -1,6 +1,4 @@
-$neutron_l2_plugin = '%(CONFIG_NEUTRON_L2_PLUGIN)s'
-
-if $neutron_l2_plugin == 'ml2' {
+if '%(CONFIG_NEUTRON_L2_PLUGIN)s' == 'ml2' {
   $agent_service = 'neutron-ovs-agent-service'
 } else {
   $agent_service = 'neutron-plugin-ovs-service'
