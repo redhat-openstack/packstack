1575898829309e76524b5f705d17eda6c3f56aff
Martin Magr <mmagr@redhat.com>
2014-08-11 15:34:17 +0200

    Remove ServerAlias definitions
    Parameter fqdn of class horizon is used both for ALLOWED_HOSTS in settings_local.py
    and for ServerAlias directives in vhost.conf which is breaking server accessibility.
    We need ALLOWED_HOSTS values, but we have to avoid ServerAlias definitions.
    For now we will use this wildcard hack until puppet-horizon will have separate
    parameter for each config.
    
    Change-Id: I77f1baa4f9c0d94b00294f17978d8383a2386d7a
    Fixes: rhbz#1119920

--- packstack/puppet/templates/horizon.pp
+++ packstack/puppet/templates/horizon.pp
@@ -8,18 +8,26 @@ package {$horizon_packages:
 }
 
 class {'horizon':
-   secret_key => '%(CONFIG_HORIZON_SECRET_KEY)s',
-   keystone_host => '%(CONFIG_CONTROLLER_HOST)s',
-   keystone_default_role => '_member_',
-   fqdn => ['%(CONFIG_CONTROLLER_HOST)s', "$::fqdn", 'localhost'],
-   can_set_mount_point => 'False',
-   django_debug => %(CONFIG_DEBUG_MODE)s ? {true => 'True', false => 'False'},
-   listen_ssl => %(CONFIG_HORIZON_SSL)s,
-   horizon_cert => '/etc/pki/tls/certs/ssl_ps_server.crt',
-   horizon_key => '/etc/pki/tls/private/ssl_ps_server.key',
-   horizon_ca => '/etc/pki/tls/certs/ssl_ps_chain.crt',
-   neutron_options => { 'enable_lb' => %(CONFIG_HORIZON_NEUTRON_LB)s,
-    'enable_firewall' => %(CONFIG_HORIZON_NEUTRON_FW)s },
+  secret_key => '%(CONFIG_HORIZON_SECRET_KEY)s',
+  keystone_host => '%(CONFIG_CONTROLLER_HOST)s',
+  keystone_default_role => '_member_',
+  #fqdn => ['%(CONFIG_CONTROLLER_HOST)s', "$::fqdn", 'localhost'],
+  # TO-DO: Parameter fqdn is used both for ALLOWED_HOSTS in settings_local.py
+  #        and for ServerAlias directives in vhost.conf which is breaking server
+  #        accessibility. We need ALLOWED_HOSTS values, but we have to avoid
+  #        ServerAlias definitions. For now we will use this wildcard hack until
+  #        puppet-horizon will have separate parameter for each config.
+  fqdn => '*',
+  can_set_mount_point => 'False',
+  django_debug => %(CONFIG_DEBUG_MODE)s ? {true => 'True', false => 'False'},
+  listen_ssl => %(CONFIG_HORIZON_SSL)s,
+  horizon_cert => '/etc/pki/tls/certs/ssl_ps_server.crt',
+  horizon_key => '/etc/pki/tls/private/ssl_ps_server.key',
+  horizon_ca => '/etc/pki/tls/certs/ssl_ps_chain.crt',
+  neutron_options => {
+    'enable_lb' => %(CONFIG_HORIZON_NEUTRON_LB)s,
+    'enable_firewall' => %(CONFIG_HORIZON_NEUTRON_FW)s
+  },
 }
 
 if %(CONFIG_HORIZON_SSL)s {
@@ -63,4 +71,3 @@ if ($::selinux != "false"){
         persistent => true,
     }
 }
-
