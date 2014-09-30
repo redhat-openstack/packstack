b9cf4bf7e56aea4669dc9b92e52ed4e0c694a32a
Gael Chamoulaud <gchamoul@redhat.com>
2014-09-30 15:33:08 +0200

    Set up NOVNC with https when using SSL on HORIZON
    Change-Id: I98d053f2d1558f39fe116a7f90bc6f1b6a4f93f4
    Fixes: rhbz#1115896
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- packstack/puppet/templates/nova_vncproxy.pp
+++ packstack/puppet/templates/nova_vncproxy.pp
@@ -1,3 +1,13 @@
+$is_using_ssl_on_horizon = '%(CONFIG_HORIZON_SSL)s'
+
+if $is_using_ssl_on_horizon == 'y' {
+  nova_config {
+    'DEFAULT/ssl_only': value => 'true';
+    'DEFAULT/cert':     value => '/etc/nova/nova.crt';
+    'DEFAULT/key':      value => '/etc/nova/nova.key';
+  }
+}
+
 class {"nova::vncproxy":
     enabled => true,
 }
