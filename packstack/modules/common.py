e18a706c99f2b5ce393fbb31aaf97eb1a891ef02
Martin Mágr <mmagr@redhat.com>
2014-10-10 14:31:05 +0200

    Fixed typo
    One line was forgotten during MySQL->MariaDB rename, which caused
    failures for deployments when MariaDB should be installed
    on separate host.
    
    Change-Id: I5b4268868c0f811d9d4aca96aabb4ea120a86285
    Fixes: rhbz#1150348

--- packstack/modules/common.py
+++ packstack/modules/common.py
@@ -6,14 +6,14 @@ from ..installer import utils
 def filtered_hosts(config, exclude=True, dbhost=True):
     """
     Returns list of hosts which need installation taking into account
-    CONFIG_MYSQL_INSTAL if parameter dbhost is True and EXCLUDE_SERVERS
+    CONFIG_MARIADB_INSTALL if parameter dbhost is True and EXCLUDE_SERVERS
     if parameter exclude is True.
     """
     exclset = set([i.strip()
                    for i in config.get('EXCLUDE_SERVERS', '').split(',')
                    if i.strip()])
     result = set()
-    dbinst = config.get('CONFIG_MYSQL_INSTALL') == 'y'
+    dbinst = config.get('CONFIG_MARIADB_INSTALL') == 'y'
     vcenter = config.get('CONFIG_VMWARE_BACKEND') == 'y'
     for hosttype, hostname in utils.host_iter(config):
         # if dbhost is being taken into account and we are not installing MySQL
