22616aabefbb3bbdcc8efd34a26b95ad42648c93
Martin Mágr <mmagr@redhat.com>
2014-10-13 14:00:08 +0200

    Reverts InnoDB performance tuning patches
    For cases where instance has really big memory (32GB) InnoDB log data
    files are quite large (approx. 6.3GB) and service start will timeout (not via Puppet).
    
    This reverts commit b6f41f021bdd488aaaea138cd66a0c5cd8a13361.
    This reverts commit 561a30af5cc383174f0b9a286b9bf3c22cc12a84.
    This reverts commit d8248b97ba73eb83322191a32efbb583928edf60.
    This reverts commit 8900f9cb842966c3e35219b897550d947153df58.
    
    Change-Id: Ibb93881d35e29cf5cae7679d885a26d3d3683a85

--- packstack/puppet/templates/mysql_install.pp
+++ packstack/puppet/templates/mysql_install.pp
@@ -1,28 +1,8 @@
 
-# on EL6 we need to wait for innodb changes before starting mysqld
-if $::operatingsystem in ['RedHat','Centos','Scientific'] and $::operatingsystemrelease < 7 {
-    $manage_service = false
-    service { 'mysqld':
-      enable  => true,
-      ensure  => 'running',
-      require => [ Package['mysql-server'], File['/etc/my.cnf'] ],
-      before  => Exec['set_mysql_rootpw'],
-    }
-    $includedir = '/etc/mysql/conf.d'
-} else {
-    $manage_service = true
-    $includedir = '/etc/my.cnf.d'
-}
-
 class {"mysql::server":
-    manage_service => $manage_service,
     config_hash => {bind_address => "0.0.0.0",
                     default_engine => "InnoDB",
                     root_password => "%(CONFIG_MYSQL_PW)s",}
-}
-
-class { "packstack::innodb":
-    includedir => "$includedir",
 }
 
 # deleting database users for security
