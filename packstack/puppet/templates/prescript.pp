86a166a4718ae90eab78949f82f4f7c12ded2a6c
Javier Pena <jpena@redhat.com>
2014-11-25 16:20:02 +0100

    Configure TCP keepalive setting to all nodes
    In some cases, it has been observed that services keep their
    connection to RabbitMQ when they should not (e.g. during a
    controller server hard reboot), creating instability. This patch
    aims at fixing this by setting TCP keepalive parameters for
    RabbitMQ connections, and setting kernel parameters to send
    keepalive packets more often.
    
    Change-Id: I7688a86e4ff617f20d6767f156c5d880e0d4d619
    Fixes: bz#1167414

--- packstack/puppet/templates/prescript.pp
+++ packstack/puppet/templates/prescript.pp
@@ -32,3 +32,17 @@ service { 'auditd':
   enable => true,
 }
 
+# The following kernel parameters help alleviate some RabbitMQ
+# connection issues
+
+sysctl::value { 'net.ipv4.tcp_keepalive_intvl':
+  value => '1',
+}
+
+sysctl::value { 'net.ipv4.tcp_keepalive_probes':
+  value => '5',
+}
+
+sysctl::value { 'net.ipv4.tcp_keepalive_time':
+  value => '5',
+}
