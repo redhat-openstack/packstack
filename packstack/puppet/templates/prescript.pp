cd76075a141a512dbb134028a15cc2b7be01012f
Christian Berendt <berendt@b1-systems.de>
2014-12-01 19:06:40 +0100

    Remove RHEL6 specific code in prescript.pp
    Because there are no more EPEL6 repositories on
    https://repos.fedorapeople.org/repos/openstack/openstack-juno/
    this code can be removed.
    
    Change-Id: I2b212ac1121f27dbc943c86610d14b6bbb52d7b3

--- packstack/puppet/templates/prescript.pp
+++ packstack/puppet/templates/prescript.pp
@@ -5,22 +5,10 @@ include firewall
 #   include packstack_apache_common
 include ::apache
 
-$el_releases = ['RedHat', 'CentOS', 'Scientific']
-
 # We don't have openstack-selinux package for Fedora
 if $::operatingsystem != 'Fedora' {
   package{ 'openstack-selinux':
     ensure => present,
-  }
-}
-
-# For older RHEL-6 releases kernel/iptools does not support netns
-if $::operatingsystem in $el_releases and $::operatingsystemmajrelease < 7 {
-  $info = "The RDO kernel that includes network namespace (netns) support has been installed on host ${::ipaddress}."
-  $warning = " This is a community supplied kernel and is not officially supported by Red Hat. Installing this kernel on RHEL systems may impact your ability to get support from Red Hat."
-
-  class { 'packstack::netns':
-    warning => "${info}${warning}"
   }
 }
 
