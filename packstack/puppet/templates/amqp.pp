c3828202e73e2fe3063f25ca267f38b297ab933d
Lukas Bezdicka <lbezdick@redhat.com>
2014-12-18 13:54:02 +0100

    [Rabbitmq] allow non-localhost connections to 'guest' user
    From fedora 22+ rabbitmq is less permissive on guest user,
    we have to allow access on this user from other hosts.
    
    Change-Id: Ic94d97c146b151cde11bcc62e1ce14703b433053
    Resolves: rhbz#1175428

--- packstack/puppet/templates/amqp.pp
+++ packstack/puppet/templates/amqp.pp
@@ -30,7 +30,8 @@ define enable_rabbitmq {
     package_provider    => 'yum',
     admin_enable        => false,
     config_variables    => {
-        'tcp_listen_options'  => "[binary,{packet, raw},{reuseaddr, true},{backlog, 128},{nodelay, true},{exit_on_close, false},{keepalive, true}]"
+        'tcp_listen_options' => "[binary,{packet, raw},{reuseaddr, true},{backlog, 128},{nodelay, true},{exit_on_close, false},{keepalive, true}]",
+        'loopback_users'     => "[]",
     }
   }
 
