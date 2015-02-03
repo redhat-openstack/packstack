513852553479da717c5c87cca76c38c5fc95b82e
Ivan Chavero <ichavero@redhat.com>
2015-02-03 10:01:40 -0700

    Fix missing heat_stack_owner role
    The configure_delegated_roles has to be set to true in order for this
    role to be created.
    
    Change-Id: I856ddaab56001c01a926ee9ed60ce4a2e596e3f1

--- packstack/puppet/templates/keystone_heat.pp
+++ packstack/puppet/templates/keystone_heat.pp
@@ -1,10 +1,11 @@
 # heat::keystone::auth
 class { 'heat::keystone::auth':
-  region           => hiera('CONFIG_KEYSTONE_REGION'),
-  password         => hiera('CONFIG_HEAT_KS_PW'),
-  public_address   => hiera('CONFIG_CONTROLLER_HOST'),
-  admin_address    => hiera('CONFIG_CONTROLLER_HOST'),
-  internal_address => hiera('CONFIG_CONTROLLER_HOST'),
+  region                    => hiera('CONFIG_KEYSTONE_REGION'),
+  password                  => hiera('CONFIG_HEAT_KS_PW'),
+  public_address            => hiera('CONFIG_CONTROLLER_HOST'),
+  admin_address             => hiera('CONFIG_CONTROLLER_HOST'),
+  internal_address          => hiera('CONFIG_CONTROLLER_HOST'),
+  configure_delegated_roles => true,
 }
 
 $is_heat_cfn_install = hiera('CONFIG_HEAT_CFN_INSTALL')
