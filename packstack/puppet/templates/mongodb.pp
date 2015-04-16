3ff9704d28789ef8519d4e8a69248e4f156a885f
Javier Pena <jpena@redhat.com>
2015-04-16 12:47:06 +0200

    Explicitly set MongoDB config file
    Depending on the combination of Linux and OpenStack distros, we
    now have different configuration files for MongoDB. Given it can
    create issues because different mongodb-server 2.6 packages have
    different configuration files, we are explicitly defining the
    config file name:
    
    - Fedora 21 with RDO: /etc/mongodb.conf
    - CentOS 7 and RHEL 7 with EPEL and RDO: /etc/mongod.conf
    - RHEL 7 with RHEL OSP: /etc/mongodb.conf
    
    The differences also affect the sharding configuration file
    (mongos.conf or mongodb-shard.conf), but Packstack does not
    configure sharding.
    
    Cherry-picked from 6ad268f49a263600a40d4720b3fa66957527524b
    
    Fixes: rhbz#1212174
    Change-Id: If138a5bcd916856090b2716bc17d5e910fa325ee

--- packstack/puppet/templates/mongodb.pp
+++ packstack/puppet/templates/mongodb.pp
@@ -1,7 +1,19 @@
 $mongodb_host = hiera('CONFIG_MONGODB_HOST')
+$use_epel = hiera('CONFIG_USE_EPEL')
+
+# The MongoDB config files differ between versions
+if (($::operatingsystem == 'fedora' and versioncmp($::operatingsystemrelease, '22') >= 0)
+    or
+    ($::operatingsystem != 'fedora' and versioncmp($::operatingsystemrelease, '7.0') >= 0 and $use_epel == 'y')
+   ){
+  $config_file = '/etc/mongod.conf'
+} else {
+  $config_file = '/etc/mongodb.conf'
+}
 
 class { 'mongodb::server':
   smallfiles => true,
-  bind_ip    => [$mongodb_host],
+  bind_ip    => $mongodb_host,
+  config     => $config_file,
 }
 
