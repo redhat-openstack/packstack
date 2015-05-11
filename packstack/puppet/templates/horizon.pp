309b36faea2e4eb29622c0a55fadae7f8634629f
Lukas Bezdicka <lbezdick@redhat.com>
2015-05-11 15:02:19 +0200

    [memcached] fix memory consumption and add temporary IPv6 hack
    - memcached can't bind to IPv6 address so we just give it localhost6
    - memcached is eating all the memory if it does not get max_memory setting
    
    Change-Id: I974733dd77c3ac7ae5f91a95132ff1ed006e846c
    Closes-Bug: 1434612
    Closes-Bug: rhbz#1185652

--- packstack/puppet/templates/horizon.pp
+++ packstack/puppet/templates/horizon.pp
@@ -46,8 +46,16 @@ if $horizon_ssl {
   apache::listen { '443': }
 }
 
+# hack for memcached, for now we bind to localhost on ipv6
+# https://bugzilla.redhat.com/show_bug.cgi?id=1210658
+$memcached_bind_host = hiera('CONFIG_IP_VERSION') ? {
+  'ipv6' => 'localhost6',
+  'ipv4' => '0.0.0.0',
+}
+
 class { '::memcached':
-  listen_ip => $bind_host,
+  listen_ip  => $memcached_bind_host,
+  max_memory => '10%%',
 }
 
 $firewall_port = hiera('CONFIG_HORIZON_PORT')
--- packstack/puppet/templates/swift_proxy.pp
+++ packstack/puppet/templates/swift_proxy.pp
@@ -6,8 +6,16 @@ $bind_host = hiera('CONFIG_IP_VERSION') ? {
   'ipv4' => '0.0.0.0',
 }
 
+# hack for memcached, for now we bind to localhost on ipv6
+# https://bugzilla.redhat.com/show_bug.cgi?id=1210658
+$memcached_bind_host = hiera('CONFIG_IP_VERSION') ? {
+  'ipv6' => 'localhost6',
+  'ipv4' => '0.0.0.0',
+}
+
 class { '::memcached':
-  listen_ip => $bind_host,
+  listen_ip  => $memcached_bind_host,
+  max_memory => '10%%',
 }
 
 class { '::swift::proxy':
