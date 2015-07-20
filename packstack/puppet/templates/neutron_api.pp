34cf1e3a89d3060ba094fab9cd0aadb2d4b42cf3
Ivan Chavero <ichavero@redhat.com>
2015-07-20 12:11:28 -0600

    Sync DB when neutron service is enabled
    When a neutron service like fwaas is enabled a db sync is
    necesary.
    
    Fixes: rhbz#1230199
    
    Change-Id: I3d8e511757b38f18f07e7e00c16a7ee83d93e283

--- packstack/puppet/templates/neutron_api.pp
+++ packstack/puppet/templates/neutron_api.pp
@@ -1,8 +1,18 @@
+
+if hiera_array('SERVICE_PLUGINS') != undef {
+  $service_sync_db = true
+}
+else {
+  $service_sync_db = false
+}
+
+
 class { '::neutron::server':
   database_connection => $neutron_sql_connection,
   auth_password       => $neutron_user_password,
   auth_uri            => hiera('CONFIG_KEYSTONE_PUBLIC_URL'),
   identity_uri        => hiera('CONFIG_KEYSTONE_ADMIN_URL'),
+  sync_db             => $service_sync_db,
   enabled             => true,
 }
 
