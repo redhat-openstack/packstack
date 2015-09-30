a0ceabfe1c8c8974d3062d5f95e4cbe312d8dff8
Lukas Bezdicka <lbezdick@redhat.com>
2015-09-30 14:18:23 +0200

    Remove deprecated configure_delegated_roles param from heat::engine
    Change-Id: I2fcab524f7445068e7798b3fe47e489e74d61108

--- packstack/puppet/templates/heat.pp
+++ packstack/puppet/templates/heat.pp
@@ -9,7 +9,6 @@ class { '::heat::engine':
   heat_waitcondition_server_url => "http://${heat_cfg_ctrl_host}:8000/v1/waitcondition",
   heat_watch_server_url         => "http://${heat_cfg_ctrl_host}:8003",
   auth_encryption_key           => hiera('CONFIG_HEAT_AUTH_ENC_KEY'),
-  configure_delegated_roles     => false,
 }
 
 keystone_user_role { "${keystone_admin}@admin":
