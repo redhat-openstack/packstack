85140643078c5c376a251bc66d8f22dd5b0f0004
Lukas Bezdicka <lbezdick@redhat.com>
2015-11-11 13:47:48 +0100

    Complete refactor of tempest provisioning
    This refactor is an attempt at providing a properly configured
    tempest installation that is ready to run out of the box as per
    the mission of packstack demo features.
    
    This commit:
    - Configures the availability of services based on configuration.
      Before, some service availability was hardcoded into the
      manifest template, this is fixed by looking up whether or not
      each service is provisioned/installed now.
    
    - Streamlines resource usage or provisioning based on requirements.
      If packstack provisions the demo resources, the tempest
      installation will be configured to use them and only provision
      the extra resources it needs.
      Otherwise, it essentially provisions what the demo provision
      does.
    
    - Adds support for keystone v3.
      Keystone v3 is enabled by default, tempest is now configured
      to use it.
    
    - Removed and cleaned up a lot of code aiming for simplicity
    
    - Adds tempest host option to allow deployment on standalone node.
    
    - Depends on https://review.openstack.org/#/c/242052/
    
    Co-Authored-By: Lukas Bezdicka <lbezdick@redhat.com>
    Change-Id: I5d69fc70ce562aaf59de5c4835d398ff4a608654

--- packstack/puppet/templates/provision_tempest.pp
+++ packstack/puppet/templates/provision_tempest.pp
@@ -1,135 +1,115 @@
-$provision_tempest_user = hiera('CONFIG_PROVISION_TEMPEST_USER')
-$provision_demo         = hiera('CONFIG_PROVISION_DEMO')
-
-if $provision_tempest_user != '' {
-  ## Keystone
-  # non admin user
-  $username                  = $provision_tempest_user
-
-  if $provision_tempest_user == 'demo' and
-    $provision_demo == 'y' {
-    $password = hiera('CONFIG_KEYSTONE_DEMO_PW')
-  } else {
-    $password = hiera('CONFIG_PROVISION_TEMPEST_USER_PW')
-  }
-
-  $tenant_name               = $provision_tempest_user
-  # admin user
-  $admin_username            = hiera('CONFIG_KEYSTONE_ADMIN_USERNAME')
-  $admin_password            = hiera('CONFIG_KEYSTONE_ADMIN_PW')
-  $admin_tenant_name         = 'admin'
-
-  ## Glance
-  $image_name               = hiera('CONFIG_PROVISION_IMAGE_NAME')
-  $image_source             = hiera('CONFIG_PROVISION_IMAGE_URL')
-  $image_ssh_user           = hiera('CONFIG_PROVISION_IMAGE_SSH_USER')
-  $image_format             = hiera('CONFIG_PROVISION_IMAGE_FORMAT')
-
-  ## Neutron
-  $public_network_name       = 'public'
-  $public_subnet_name        = 'public_subnet'
-  $floating_range            = hiera('CONFIG_PROVISION_DEMO_FLOATRANGE')
-  $private_network_name      = 'private'
-  $private_subnet_name       = 'private_subnet'
-  $fixed_range               = '10.0.0.0/24'
-  $router_name               = 'router1'
-
-  ## Tempest
-  $configure_tempest         = hiera('CONFIG_PROVISION_TEMPEST')
-
-  $image_name_alt            = false
-  $image_source_alt          = false
-  $image_ssh_user_alt        = false
-
-  $identity_uri              = undef
-  $tempest_repo_uri          = hiera('CONFIG_PROVISION_TEMPEST_REPO_URI')
-  $tempest_repo_revision     = hiera('CONFIG_PROVISION_TEMPEST_REPO_REVISION')
-  $tempest_clone_path        = '/var/lib/tempest'
-  $tempest_clone_owner       = 'root'
-  $setup_venv                = false
-  $resize_available          = undef
-  $change_password_available = undef
-  $cinder_available          = undef
-  $glance_available          = true
-  $heat_available            = undef
-  $horizon_available         = undef
-  $neutron_available         = hiera('PROVISION_NEUTRON_AVAILABLE')
-  $nova_available            = true
-  $swift_available           = undef
-
-  # Support creation of a second glance image
-  # distinct from the first, for tempest. It
-  # doesn't need to be a different image, just
-  # have a different name and ref in glance.
-  if $image_name_alt {
-    $image_name_alt_real = $image_name_alt
-    if ! $image_source_alt {
-      # Use the same source by default
-      $image_source_alt_real = $image_source
-    } else {
-      $image_source_alt_real = $image_source_alt
-    }
-
-    if ! $image_ssh_user_alt {
-      # Use the same user by default
-      $image_alt_ssh_user_real = $image_ssh_user
-    } else {
-      $image_alt_ssh_user_real = $image_ssh_user_alt
-    }
-
-  } else {
-    $image_name_alt_real = $image_name
-  }
-
-  ## Tempest
-
-  if $configure_tempest {
-    class { '::tempest':
-      tempest_repo_uri          => $tempest_repo_uri,
-      tempest_clone_path        => $tempest_clone_path,
-      tempest_clone_owner       => $tempest_clone_owner,
-      setup_venv                => $setup_venv,
-      tempest_repo_revision     => $tempest_repo_revision,
-      image_name                => $image_name,
-      image_name_alt            => $image_name_alt_real,
-      image_ssh_user            => $image_ssh_user,
-      image_alt_ssh_user        => $image_alt_ssh_user_real,
-      identity_uri              => $identity_uri,
-      username                  => $username,
-      password                  => $password,
-      tenant_name               => $tenant_name,
-      admin_username            => $admin_username,
-      admin_password            => $admin_password,
-      admin_tenant_name         => $admin_tenant_name,
-      public_network_name       => $public_network_name,
-      resize_available          => $resize_available,
-      change_password_available => $change_password_available,
-      cinder_available          => $cinder_available,
-      glance_available          => $glance_available,
-      heat_available            => $heat_available,
-      horizon_available         => $horizon_available,
-      neutron_available         => $neutron_available,
-      nova_available            => $nova_available,
-      swift_available           => $swift_available,
-      require                   => $tempest_requires,
-    }
-  }
+$provision_demo            = str2bool(hiera('CONFIG_PROVISION_DEMO'))
+if $provision_demo {
+  $username             = 'demo'
+  $password             = hiera('CONFIG_KEYSTONE_DEMO_PW')
+  $tenant_name          = 'demo'
+  $floating_range       = hiera('CONFIG_PROVISION_DEMO_FLOATRANGE')
 } else {
-  ## Standalone Tempest installation
-  class { '::tempest':
-    tempest_repo_uri      => hiera('CONFIG_PROVISION_TEMPEST_REPO_URI'),
-    tempest_clone_path    => '/var/lib/tempest',
-    tempest_clone_owner   => 'root',
-    setup_venv            => false,
-    tempest_repo_revision => hiera('CONFIG_PROVISION_TEMPEST_REPO_REVISION'),
-    configure_images      => false,
-    configure_networks    => false,
-    cinder_available      => undef,
-    glance_available      => true,
-    heat_available        => undef,
-    horizon_available     => undef,
-    neutron_available     => false,
-    nova_available        => true,
-    swift_available       => undef,
-  }
+  $username             = hiera('CONFIG_PROVISION_TEMPEST_USER')
+  $password             = hiera('CONFIG_PROVISION_TEMPEST_USER_PW')
+  $tenant_name          = 'tempest'
+  $floating_range       = hiera('CONFIG_PROVISION_TEMPEST_FLOATRANGE')
 }
+
+# Authentication/Keystone
+$identity_uri          = hiera('CONFIG_KEYSTONE_PUBLIC_URL')
+$identity_uri_v3       = regsubst($identity_uri, 'v2.0', 'v3')
+$auth_version          = regsubst(hiera('CONFIG_KEYSTONE_API_VERSION'), '.0', '')
+$admin_username        = hiera('CONFIG_KEYSTONE_ADMIN_USERNAME')
+$admin_password        = hiera('CONFIG_KEYSTONE_ADMIN_PW')
+$admin_tenant_name     = 'admin'
+$admin_domain_name     = 'Default'
+
+# get image and network id
+$configure_images          = true
+$configure_networks        = true
+
+# Image
+$image_name         = hiera('CONFIG_PROVISION_IMAGE_NAME')
+$image_ssh_user     = hiera('CONFIG_PROVISION_IMAGE_SSH_USER')
+$image_name_alt     = "${image_name}_alt"
+$image_alt_ssh_user = hiera('CONFIG_PROVISION_IMAGE_SSH_USER')
+$image_source       = hiera('CONFIG_PROVISION_IMAGE_URL')
+$image_format       = hiera('CONFIG_PROVISION_IMAGE_FORMAT')
+
+# network name
+$public_network_name = 'public'
+
+# nova should be able to resize with packstack setup
+$resize_available          = true
+
+$change_password_available = undef
+$allow_tenant_isolation    = true
+$log_file                  = 'tempest.log'
+$use_stderr                = false
+$debug                     = true
+$public_router_id          = undef
+
+# Tempest
+$tempest_repo_uri      = hiera('CONFIG_PROVISION_TEMPEST_REPO_URI')
+$tempest_repo_revision = hiera('CONFIG_PROVISION_TEMPEST_REPO_REVISION')
+$tempest_clone_path    = '/var/lib/tempest'
+$tempest_clone_owner   = 'root'
+$setup_venv            = true
+$tempest_user          = hiera('CONFIG_PROVISION_TEMPEST_USER')
+$tempest_password      = hiera('CONFIG_PROVISION_TEMPEST_USER_PW')
+
+# Service availability for testing based on configuration
+$cinder_available     = str2bool(hiera('CONFIG_CINDER_INSTALL'))
+$glance_available     = str2bool(hiera('CONFIG_GLANCE_INSTALL'))
+$horizon_available    = str2bool(hiera('CONFIG_HORIZON_INSTALL'))
+$nova_available       = str2bool(hiera('CONFIG_NOVA_INSTALL'))
+$neutron_available    = str2bool(hiera('CONFIG_NEUTRON_INSTALL'))
+$ceilometer_available = str2bool(hiera('CONFIG_CEILOMETER_INSTALL'))
+$trove_available      = str2bool(hiera('CONFIG_TROVE_INSTALL'))
+$sahara_available     = str2bool(hiera('CONFIG_SAHARA_INSTALL'))
+$heat_available       = str2bool(hiera('CONFIG_HEAT_INSTALL'))
+$swift_available      = str2bool(hiera('CONFIG_SWIFT_INSTALL'))
+$configure_tempest    = str2bool(hiera('CONFIG_PROVISION_TEMPEST'))
+
+class { '::tempest':
+  admin_username            => $admin_username,
+  admin_password            => $admin_password,
+  admin_tenant_name         => $admin_tenant_name,
+  admin_domain_name         => $admin_domain_name,
+  username                  => $username,
+  tenant_name               => $tenant_name,
+  password                  => $password,
+  identity_uri              => $identity_uri,
+  identity_uri_v3           => $identity_uri_v3,
+  auth_version              => $auth_version,
+  configure_images          => $configure_images,
+  configure_networks        => $configure_networks,
+  image_name                => $image_name,
+  image_ssh_user            => $image_ssh_user,
+  image_name_alt            => $image_name_alt,
+  image_alt_ssh_user        => $image_alt_ssh_user,
+  public_network_name       => $public_network_name,
+  resize_available          => $resize_available,
+  change_password_available => $change_password_available,
+  allow_tenant_isolation    => $allow_tenant_isolation,
+  log_file                  => $log_file,
+  use_stderr                => $use_stderr,
+  debug                     => $debug,
+  public_router_id          => $public_router_id,
+  glance_available          => $glance_available,
+  horizon_available         => $horizon_available,
+  nova_available            => $nova_available,
+  neutron_available         => $neutron_available,
+  ceilometer_available      => $ceilometer_available,
+  trove_available           => $trove_available,
+  sahara_available          => $sahara_available,
+  heat_available            => $heat_available,
+  swift_available           => $swift_available,
+  tempest_repo_uri          => $tempest_repo_uri,
+  tempest_clone_path        => $tempest_clone_path,
+  tempest_clone_owner       => $tempest_clone_owner,
+  tempest_repo_revision     => $tempest_repo_revision,
+  setup_venv                => $setup_venv,
+}
+
+tempest_config { 'object-storage/operator_role':
+  value => 'SwiftOperator',
+  path  => "${tempest_clone_path}/etc/tempest.conf",
+}
+
