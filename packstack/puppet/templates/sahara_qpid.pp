3e727b5f1cdff3e0e80f40b3b9b70ce06caba7a9
Javier Pena <jpena@redhat.com>
2015-12-16 17:49:21 +0100

    Fix Sahara templates to work with latest puppet-sahara
    The latest puppet-sahara module introduces a few incompatibilities:
    
    - It enables $::os_service_default (https://review.openstack.org/245178),
      which requires changes in the way kombu_ssl_* parameters are
      handled.
    - It removes sahara::notify::rabbitmq and sahara::notify::qpid
      (https://review.openstack.org/253099), so we need to configure
      the rpc backend in a different way.
    - It also removes all qpid functionality
      (https://review.openstack.org/253105). It is kept for now, so it
      can be properly removed with a single review.
    
    Change-Id: I5608691776566700386cc379ce296fb510358d50

--- packstack/puppet/templates/sahara_qpid.pp
+++ packstack/puppet/templates/sahara_qpid.pp
@@ -1,7 +1,23 @@
-class { '::sahara::notify::qpid':
-  qpid_hostname => hiera('CONFIG_AMQP_HOST_URL'),
-  qpid_port     => hiera('CONFIG_AMQP_CLIENTS_PORT'),
-  qpid_protocol => hiera('CONFIG_AMQP_PROTOCOL'),
-  qpid_username => hiera('CONFIG_AMQP_AUTH_USER'),
-  qpid_password => hiera('CONFIG_AMQP_AUTH_PASSWORD'),
+$sahara_cfg_sahara_db_pw = hiera('CONFIG_SAHARA_DB_PW')
+$sahara_cfg_sahara_mariadb_host = hiera('CONFIG_MARIADB_HOST_URL')
+$sahara_cfg_config_neutron_install = hiera('CONFIG_NEUTRON_INSTALL')
+
+class { '::sahara':
+  database_connection =>
+    "mysql+pymysql://sahara:${sahara_cfg_sahara_db_pw}@${sahara_cfg_sahara_mariadb_host}/sahara",
+  verbose             => true,
+  debug               => hiera('CONFIG_DEBUG_MODE'),
+  admin_user          => 'sahara',
+  admin_password      => hiera('CONFIG_SAHARA_KS_PW'),
+  admin_tenant_name   => 'services',
+  auth_uri            => hiera('CONFIG_KEYSTONE_PUBLIC_URL'),
+  identity_uri        => hiera('CONFIG_KEYSTONE_ADMIN_URL'),
+  use_neutron         => ($sahara_cfg_config_neutron_install == 'y'),
+  host                => hiera('CONFIG_SAHARA_HOST'),
+  rpc_backend         => 'qpid',
+  qpid_hostname       => hiera('CONFIG_AMQP_HOST_URL'),
+  qpid_port           => hiera('CONFIG_AMQP_CLIENTS_PORT'),
+  qpid_protocol       => hiera('CONFIG_AMQP_PROTOCOL'),
+  qpid_username       => hiera('CONFIG_AMQP_AUTH_USER'),
+  qpid_password       => hiera('CONFIG_AMQP_AUTH_PASSWORD'),
 }
