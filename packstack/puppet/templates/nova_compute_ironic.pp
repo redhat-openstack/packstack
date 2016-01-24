6cd7036bdf433fa50103c60b59429d4664bdcc91
Hiroki Aramaki <h-aramaki@netone.co.jp>
2016-01-24 15:30:15 +0900

    Add version number to admin_url parameter in nova.conf ironic section
    nova-compute process expect append version number to admin_url.
    This patch add version number to admin_url paramter in nova.conf ironic
    section.
    
    Change-Id: Ie50c2b2c6d3debdce7ca789c63118f34b58d994e
    Closes-Bug: 1537412

--- packstack/puppet/templates/nova_compute_ironic.pp
+++ packstack/puppet/templates/nova_compute_ironic.pp
@@ -1,9 +1,11 @@
 $ironic_config_controller_host = hiera('CONFIG_KEYSTONE_HOST_URL')
+$ironic_config_keystone_admin = hiera('CONFIG_KEYSTONE_ADMIN_URL')
+$ironic_config_keystone_version = hiera('CONFIG_KEYSTONE_API_VERSION')
 
 class { '::nova::compute::ironic':
   admin_user        => 'ironic',
   admin_passwd      => hiera('CONFIG_IRONIC_KS_PW'),
-  admin_url         => hiera('CONFIG_KEYSTONE_ADMIN_URL'),
+  admin_url         => "${ironic_config_keystone_admin}/${ironic_config_keystone_version}",
   admin_tenant_name => 'services',
   api_endpoint      => "http://${ironic_config_controller_host}:6385/v1",
 }
