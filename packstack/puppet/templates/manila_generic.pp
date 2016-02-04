b13fdf58333482f56fa14fb35b0df19af50e50ef
Ivan Chavero <ichavero@redhat.com>
2016-02-04 00:07:42 -0700

    Set manila nova_flavor id
    The default service_instance_flavor_id for the
    manila::service_instance class does not provide the
    disk space required for manila to work (Fixes rhbz#1304104)
    
    Change-Id: Ib7d1a5753e501f1f5d70652984e91980fc6316d0

--- packstack/puppet/templates/manila_generic.pp
+++ packstack/puppet/templates/manila_generic.pp
@@ -16,11 +16,23 @@ elsif ($::manila_network_type == 'nova-network'){
   $service_instance_network_helper_type = 'nova'
 }
 
+$admin_username = hiera('CONFIG_KEYSTONE_ADMIN_USERNAME')
+$admin_password = hiera('CONFIG_KEYSTONE_ADMIN_PW')
+$admin_tenant   = 'admin'
+$keystone_url   = hiera('CONFIG_KEYSTONE_PUBLIC_URL')
+
+exec { 'create m1.manila nova flavor':
+  path     => '/usr/bin:/bin:/usr/sbin:/sbin',
+  provider => shell,
+  command  => "nova --os-username $admin_username --os-password $admin_password --os-tenant-name $admin_tenant --os-auth-url $keystone_url flavor-create m1.manila 66 512 0 1",
+  unless   => "nova --os-username $admin_username --os-password $admin_password --os-tenant-name $admin_tenant --os-auth-url $keystone_url flavor-list | grep 66",
+} ->
 manila::service_instance{ 'generic':
   service_image_location               => hiera('CONFIG_MANILA_SERVICE_IMAGE_LOCATION'),
   service_instance_user                => hiera('CONFIG_MANILA_SERVICE_INSTANCE_USER'),
   service_instance_password            => hiera('CONFIG_MANILA_SERVICE_INSTANCE_PASSWORD'),
   service_instance_network_helper_type => $service_instance_network_helper_type,
+  service_instance_flavor_id           => 66,
 }
 
 class { '::manila::compute::nova':
