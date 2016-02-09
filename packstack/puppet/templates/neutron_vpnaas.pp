a7eb3cfa296219018f3e54434298a42e1991e669
Javier Pena <jpena@redhat.com>
2016-02-09 17:04:18 +0100

    Use the Libreswan driver for VPNaaS
    Red Hat and CentOS distribute Libreswan instead of Openswan. There
    seem to be some differences in the VPNaaS driver for each, so we
    sould use the Libreswan driver.
    
    Change-Id: I579fdd3ff670ecd695c7e1e031ec2446cde1cdef
    Fixes: rhbz#1297833

--- packstack/puppet/templates/neutron_vpnaas.pp
+++ packstack/puppet/templates/neutron_vpnaas.pp
@@ -1,3 +1,5 @@
 class { '::neutron::agents::vpnaas':
-  enabled => true,
+  enabled           => true,
+  vpn_device_driver => 'neutron_vpnaas.services.vpn.device_drivers.libreswan_ipsec.LibreSwanDriver',
 }
+
