52dfedb1a22712d759c76f360f49b383d5a53d7f
Lukas Bezdicka <lbezdick@redhat.com>
2016-02-22 11:33:45 +0100

    Switch to new stable/kilo OPM branch
    The patches branch for OPM is now stable/kilo.
    
    Change-Id: Id337109e8949a392d38f6a9141440446a8139af3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ MODULES_DIR = os.environ.get('PACKSTACK_PUPPETDIR',
                              '/usr/share/openstack-puppet/modules')
 MODULES_REPO = ('https://github.com/redhat-openstack/'
                 'openstack-puppet-modules.git')
-MODULES_BRANCH = 'f23-patches'
+MODULES_BRANCH = 'stable/kilo'
 
 
 class InstallModulesCommand(Command):
