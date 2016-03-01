1f1fb68aaf278440ef194ff127c6c18b3398864a
David Moreau Simard <dms@redhat.com>
2016-03-01 16:55:42 -0500

    Pass operator_roles to swift::keystone::auth
    Passing operator_roles to swift::keystone::auth will ensure the
    roles are created. This resolves a Tempest failure where it expects
    the ResellerAdmin role to exist.
    
    Change-Id: I83c61b52ad9a6e57f869fe6496fa269567951261

--- packstack/puppet/templates/keystone_swift.pp
+++ packstack/puppet/templates/keystone_swift.pp
@@ -5,8 +5,9 @@ $swift_url = "${swift_protocol}://${swift_host}:$swift_port/v1/AUTH_%%(tenant_id
 $swift_v3_url = "${swift_protocol}://${swift_host}:$swift_port"
 
 class { '::swift::keystone::auth':
-  region         => hiera('CONFIG_KEYSTONE_REGION'),
-  password       => hiera('CONFIG_SWIFT_KS_PW'),
+  region          => hiera('CONFIG_KEYSTONE_REGION'),
+  password        => hiera('CONFIG_SWIFT_KS_PW'),
+  operator_roles  => ['admin', 'SwiftOperator', 'ResellerAdmin'],
   public_url      => $swift_url,
   internal_url    => $swift_url,
   admin_url       => $swift_url,
