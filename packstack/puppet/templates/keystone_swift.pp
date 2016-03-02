a49c6646e258dfcf2f1cafe1431352cd952c5976
Ivan Chavero <ichavero@redhat.com>
2016-03-01 22:58:27 -0700

    Remove S3 endpoint
    The Swift S3 endpoint is being enabled without user intervention
    or configuration directive, since packstack does not suppor this
    feature it had to be removed.
    
    Change-Id: Iee381a0937266083e023e7fad69ad6c61048cf98

--- packstack/puppet/templates/keystone_swift.pp
+++ packstack/puppet/templates/keystone_swift.pp
@@ -2,4 +2,5 @@ class { '::swift::keystone::auth':
   public_address => hiera('CONFIG_STORAGE_HOST_URL'),
   region         => hiera('CONFIG_KEYSTONE_REGION'),
   password       => hiera('CONFIG_SWIFT_KS_PW'),
+  configure_s3_endpoint  => false,
 }
