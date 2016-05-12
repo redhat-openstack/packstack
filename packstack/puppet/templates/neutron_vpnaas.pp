527c2d9c24cac8ea835aa7a6beb05f0915209371
Javier Pena <jpena@redhat.com>
2016-05-12 12:53:01 +0200

    Remove workaround for VPNaaS configuration
    https://review.openstack.org/273934 was merged some time ago, so we
    can remove the workaround and use the proper code.
    
    Change-Id: I6508cee9ec98c88fd8d3a220c8a76304b0e7c732

--- packstack/puppet/templates/neutron_vpnaas.pp
+++ packstack/puppet/templates/neutron_vpnaas.pp
@@ -1,15 +1,9 @@
 class { '::neutron::agents::vpnaas':
   enabled           => true,
   vpn_device_driver => 'neutron_vpnaas.services.vpn.device_drivers.libreswan_ipsec.LibreSwanDriver',
+} ->
+class { '::neutron::services::vpnaas':
+  service_providers => 'VPN:libreswan:neutron_vpnaas.services.vpn.service_drivers.ipsec.IPsecVPNDriver:default',
+  notify            => Service['neutron-server'],
 }
 
-# FIXME: this is a workaround until
-# https://bugs.launchpad.net/puppet-neutron/+bug/1538971 is fixed
-
-file_line { 'vpnaas_service_provider':
-  path    => '/etc/neutron/neutron_vpnaas.conf',
-  match   => '^service_provider +=.*',
-  line    => 'service_provider = VPN:libreswan:neutron_vpnaas.services.vpn.service_drivers.ipsec.IPsecVPNDriver:default',
-  require => Class['::neutron::agents::vpnaas'],
-  notify  => Service['neutron-server'],
-}
