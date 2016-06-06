9af04d8480da4359f766e59c45ac23f199b138b0
Javier Peña <jpena@redhat.com>
2016-06-06 14:06:15 +0000

    Revert "Fix cinder directory permissions" and fix swift manifest
    This should not be needed anymore. Commit
    https://review.openstack.org/325334 to puppet-cinder sets the proper
    permissions.
    
    This reverts commit 8178b046156247e75234d928d5affb16caec696e.
    
    Also, change swift_hash_suffix to swift_hash_path_suffix, as
    required by upstream puppet-swift change
    https://review.openstack.org/324203 . This is included here to allow
    CI to pass, as we need the two fixes merged together (it comes from
    abandoned change https://review.openstack.org/325786).
    
    Change-Id: I0c51a38b5e8a7181689276ce307f2f91aa18a92d
    (cherry picked from commit ac8dc735a3e0f06e7ed7a1dff6ce7122cc1210fb)

--- packstack/puppet/templates/swift_common.pp
+++ packstack/puppet/templates/swift_common.pp
@@ -3,6 +3,6 @@ Class['swift'] -> Service <| |>
 
 class { '::swift':
   # not sure how I want to deal with this shared secret
-  swift_hash_suffix => hiera('CONFIG_SWIFT_HASH'),
-  package_ensure    => latest,
+  swift_hash_path_suffix => hiera('CONFIG_SWIFT_HASH'),
+  package_ensure         => latest,
 }
