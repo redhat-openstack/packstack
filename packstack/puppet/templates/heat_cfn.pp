70efc3ffad9c0b61f4472a5d511cead715877ea0
Javier Pena <jpena@redhat.com>
2016-06-10 12:35:40 +0200

    Fix Heat CFN template
    Some paramaters were deprecated by https://review.openstack.org/308511
    
    Closes-bug: #1593766
    Resolves: rhbz#1344663
    
    Change-Id: I7b02c7c8392675de5eb8fb75f0d5e989a39b356b

--- packstack/puppet/templates/heat_cfn.pp
+++ packstack/puppet/templates/heat_cfn.pp
@@ -6,8 +6,8 @@ class { '::heat::api_cfn':
 $heat_cfn_cfg_ctrl_host = hiera('CONFIG_KEYSTONE_HOST_URL')
 
 class { '::heat::keystone::auth_cfn':
-  admin_address    => $heat_cfn_cfg_ctrl_host,
-  public_address   => $heat_cfn_cfg_ctrl_host,
-  internal_address => $heat_cfn_cfg_ctrl_host,
-  password         => hiera('CONFIG_HEAT_KS_PW'),
+  admin_url    => "http://$heat_cfn_cfg_ctrl_host:8000/v1",
+  public_url   => "http://$heat_cfn_cfg_ctrl_host:8000/v1",
+  internal_url => "http://$heat_cfn_cfg_ctrl_host:8000/v1",
+  password     => hiera('CONFIG_HEAT_KS_PW'),
 }
