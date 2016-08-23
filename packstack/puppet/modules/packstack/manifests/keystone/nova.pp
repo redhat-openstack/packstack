b076dcd6ca07cabf66f61305fcb096080ac248f0
Ricardo Noriega <rnoriega@redhat.com>
2016-08-23 18:30:52 +0200

    Nova API endpoint versions
    Default endpoint version should be 2.1.
    Nova API v3 has been deprecated.
    
    Change-Id: Ic622cd1ccde9d1dab487499614cf486cb0cdb4f7

--- packstack/puppet/modules/packstack/manifests/keystone/nova.pp
+++ packstack/puppet/modules/packstack/manifests/keystone/nova.pp
@@ -3,8 +3,7 @@ class packstack::keystone::nova ()
     $nova_protocol = 'http'
     $nova_host = hiera('CONFIG_KEYSTONE_HOST_URL')
     $nova_port = '8774'
-    $nova_url = "${nova_protocol}://${nova_host}:$nova_port/v2/%(tenant_id)s"
-    $nova_v3_url = "${nova_protocol}://${nova_host}:$nova_port/v3"
+    $nova_url = "${nova_protocol}://${nova_host}:$nova_port/v2.1/%(tenant_id)s"
 
     class { '::nova::keystone::auth':
       region          => hiera('CONFIG_KEYSTONE_REGION'),
@@ -12,8 +11,5 @@ class packstack::keystone::nova ()
       public_url      => $nova_url,
       admin_url       => $nova_url,
       internal_url    => $nova_url,
-      public_url_v3   => $nova_v3_url,
-      admin_url_v3    => $nova_v3_url,
-      internal_url_v3 => $nova_v3_url,
     }
 }
