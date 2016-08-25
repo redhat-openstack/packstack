a8128aebd20c17a304840c3b10bd32729accad55
Javier Pena <jpena@redhat.com>
2016-08-25 10:52:19 +0200

    Use lbaasv2 instead of v1
    The upstream neutron-lbaas project has removed lbaasv1
    (https://review.openstack.org/286381). Let's switch Packstack support
    to v2.
    
    Change-Id: I249b177616d14273d59721af5eccb38980f7b50a

--- packstack/puppet/modules/packstack/manifests/neutron/lbaas.pp
+++ packstack/puppet/modules/packstack/manifests/neutron/lbaas.pp
@@ -2,8 +2,10 @@ class packstack::neutron::lbaas ()
 {
     class { '::neutron::agents::lbaas':
       interface_driver => hiera('CONFIG_NEUTRON_LBAAS_INTERFACE_DRIVER'),
-      device_driver    => 'neutron.services.loadbalancer.drivers.haproxy.namespace_driver.HaproxyNSDriver',
+      device_driver    => 'neutron_lbaas.services.loadbalancer.drivers.haproxy.namespace_driver.HaproxyNSDriver',
       user_group       => 'haproxy',
+      enable_v1        => false,
+      enable_v2        => true,
       debug            => hiera('CONFIG_DEBUG_MODE'),
     }
 
