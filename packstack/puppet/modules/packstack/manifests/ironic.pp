92ea78df62843290865e9f03571f83189366d3ff
Ivan Chavero <ichavero@redhat.com>
2016-09-09 15:15:04 -0600

    Change ironic identity to authtoken
    https://review.openstack.org/#/c/346028/14 moved keystone resource
    creation to ironic::keystone::authtoken. Adapting packstack to it.
    
    Change-Id: I22cb0b9fdbfa31aa58d5e25c28b44b4a1b95bd48

--- packstack/puppet/modules/packstack/manifests/ironic.pp
+++ packstack/puppet/modules/packstack/manifests/ironic.pp
@@ -6,10 +6,12 @@ class packstack::ironic ()
       'glance/glance_host': value => hiera('CONFIG_STORAGE_HOST_URL');
     }
 
-    class { '::ironic::api':
-      auth_uri       => hiera('CONFIG_KEYSTONE_PUBLIC_URL'),
-      admin_password => hiera('CONFIG_IRONIC_KS_PW'),
+    class { '::ironic::api::authtoken': {
+      auth_uri => hiera('CONFIG_KEYSTONE_PUBLIC_URL'),
+      password => hiera('CONFIG_IRONIC_KS_PW'),
     }
+
+    class { '::ironic::api': }
 
     class { '::ironic::client': }
 
