182064df9f3c21a65437251c62cc43106a1d9cef
Javier Pena <jpena@redhat.com>
2016-09-21 17:02:11 +0200

    Fix Neutron lbaas and add it to scenario002
    The lbaas service was moved to use lbaasv2 in https://review.openstack.org/360352.
    However, some additional fixes are needed to make it work.
    
    To ensure lbaas is working after future changes, it is now enabled
    as part of scenario002.
    
    Change-Id: I050c831041262636f07ad616bca6acaaff42ed37

--- packstack/puppet/modules/packstack/manifests/neutron/lbaas.pp
+++ packstack/puppet/modules/packstack/manifests/neutron/lbaas.pp
@@ -2,7 +2,7 @@ class packstack::neutron::lbaas ()
 {
     class { '::neutron::agents::lbaas':
       interface_driver => hiera('CONFIG_NEUTRON_LBAAS_INTERFACE_DRIVER'),
-      device_driver    => 'neutron_lbaas.services.loadbalancer.drivers.haproxy.namespace_driver.HaproxyNSDriver',
+      device_driver    => 'neutron_lbaas.drivers.haproxy.namespace_driver.HaproxyNSDriver',
       user_group       => 'haproxy',
       enable_v1        => false,
       enable_v2        => true,
@@ -10,6 +10,6 @@ class packstack::neutron::lbaas ()
     }
 
     class {'::neutron::services::lbaas':
-      service_providers => 'LOADBALANCER:Haproxy:neutron_lbaas.services.loadbalancer.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver:default',
+      service_providers => 'LOADBALANCERV2:Haproxy:neutron_lbaas.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver',
     }
 }
