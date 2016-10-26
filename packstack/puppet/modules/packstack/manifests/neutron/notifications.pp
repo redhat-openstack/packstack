53848244f6ff222591391a858970b25305cd4734
Alex Schultz <aschultz@redhat.com>
2016-10-26 09:40:54 -0600

    nova_url deprecated for neutron notifications
    The nova_url option for neutron server notifications was deprecated in
    Newton[0] and will be removed.
    
    [0] https://review.openstack.org/#/c/314735/
    
    Change-Id: I506a08f27a1ca8e7897c3de33379df9c39b9ac96

--- packstack/puppet/modules/packstack/manifests/neutron/notifications.pp
+++ packstack/puppet/modules/packstack/manifests/neutron/notifications.pp
@@ -7,7 +7,6 @@ class packstack::neutron::notifications ()
       username    => 'nova',
       password    => hiera('CONFIG_NOVA_KS_PW'),
       tenant_name => 'services',
-      nova_url    => "http://${neutron_notif_cfg_ctrl_host}:8774/v2",
       auth_url    => hiera('CONFIG_KEYSTONE_ADMIN_URL'),
       region_name => hiera('CONFIG_KEYSTONE_REGION'),
     }
