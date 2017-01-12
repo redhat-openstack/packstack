186acfee21e852f14764774a42746df7dca52c6f
Javier Pena <jpena@redhat.com>
2017-01-12 14:19:21 +0100

    Remove rabbit_use_ssl in class swift::proxy::ceilometer
    This was wrongly added in https://review.openstack.org/409205, the
    puppet-swift module does not support this parameter.
    
    Change-Id: I2e55b6317ad3b94c44a3401c793c582a5fe052a1

--- packstack/puppet/modules/packstack/manifests/swift/ceilometer.pp
+++ packstack/puppet/modules/packstack/manifests/swift/ceilometer.pp
@@ -6,7 +6,6 @@ class packstack::swift::ceilometer ()
     $rabbit_password = hiera('CONFIG_AMQP_AUTH_PASSWORD')
 
     class { '::swift::proxy::ceilometer':
-      rabbit_use_ssl        => hiera('CONFIG_AMQP_SSL_ENABLED'),
       default_transport_url => "rabbit://${rabbit_userid}:${rabbit_password}@${rabbit_host}:${rabbit_port}/",
       topic                 => 'notifications',
       control_exchange      => 'swift',
