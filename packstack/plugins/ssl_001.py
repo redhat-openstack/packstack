d8b0166146effb7c3dd1e042c32ce3d62188a42b
Javier Pena <jpena@redhat.com>
2017-06-21 15:05:53 +0200

    Fix Horizon SSL when AMQP SSL is not configured
    https://review.openstack.org/329505 commented out a couple lines
    in ssl_001.py, which effectively break SSL cert creation for Horizon
    if AMQP SSL is not configured.
    
    Change-Id: I8ef4e398f852dc3ab77fadde01777f2f136e6e37

--- packstack/plugins/ssl_001.py
+++ packstack/plugins/ssl_001.py
@@ -213,9 +213,8 @@ def create_self_signed_cert(config, messages):
     # for now hardcoded place for landing CACert file on servers
     config['CONFIG_SSL_CACERT'] = '/etc/pki/tls/certs/packstack_cacert.crt'
 
-#    if (config['CONFIG_AMQP_ENABLE_SSL'] != 'y' and
-#       config["CONFIG_HORIZON_SSL"] != 'y'):
-    if config['CONFIG_AMQP_ENABLE_SSL'] != 'y':
+    if (config['CONFIG_AMQP_ENABLE_SSL'] != 'y' and
+       config["CONFIG_HORIZON_SSL"] != 'y'):
         return
 
     config['CONFIG_SSL_CERT_DIR'] = os.path.expanduser(
