187af0d761f3fce28f1e0cd73c3dc4806992a4fd
yatin <ykarel@redhat.com>
2017-08-28 12:52:10 +0530

    Use alternative cirros url until download.cirros-cloud.net is fixed
    Change-Id: I98e3b04e18137809b6ff7806fa43de3ca672e743
    Closes-Bug: #1713358

--- run_tests.sh
+++ run_tests.sh
@@ -179,14 +179,16 @@ if [ -f ~/cache/files/cirros-0.3.4-x86_64-uec.tar.gz ]; then
     tar -xzvf ~/cache/files/cirros-0.3.4-x86_64-uec.tar.gz -C /tmp/cirros/
 else
     echo "No pre-cached uec archive found, downloading..."
-    wget --tries=10 http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-uec.tar.gz -P /tmp/cirros/
+    # FIXME(ykarel) download.cirros-cloud.net domain is expired, see
+    # https://bugs.launchpad.net/cirros/+bug/1713358
+    wget --tries=10 http://trunk.rdoproject.org/cirros-0.3.4-x86_64-uec.tar.gz -P /tmp/cirros/
     tar -xzvf /tmp/cirros/cirros-0.3.4-x86_64-uec.tar.gz -C /tmp/cirros/
 fi
 if [ -f ~/cache/files/cirros-0.3.4-x86_64-disk.img ]; then
     cp -p ~/cache/files/cirros-0.3.4-x86_64-disk.img /tmp/cirros/
 else
     echo "No pre-cached disk image found, downloading..."
-    wget --tries=10 http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img -P /tmp/cirros/
+    wget --tries=10 http://trunk.rdoproject.org/cirros-0.3.4-x86_64-disk.img -P /tmp/cirros/
 fi
 echo "Using pre-cached images:"
 find /tmp/cirros -type f -printf "%m %n %u %g %s  %t" -exec md5sum \{\} \;
