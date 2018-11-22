9c074efdd54ea632f46d3abf02ab9237793ee662
yatin <ykarel@redhat.com>
2018-11-22 11:47:13 +0530

    Set metadata_host instead of metadata_ip
    "nova_metadata_ip" option is deprecated since pike in
    favor of "nova_metadata_host", puppet-neutron accepts
    both parameter since pike, so this patch set's the non
    deprecated parameter for neutron metadata.
    
    Closes-Bug: https://bugzilla.redhat.com/show_bug.cgi?id=1649304
    Change-Id: I1c045b46cb128a45219f90b4c71ae86936e8d959
    (cherry picked from commit 952b0550f083998f376ef15c017e8892d500c853)

--- packstack/puppet/modules/packstack/manifests/neutron/metadata.pp
+++ packstack/puppet/modules/packstack/manifests/neutron/metadata.pp
@@ -2,7 +2,7 @@ class packstack::neutron::metadata ()
 {
     class { '::neutron::agents::metadata':
       shared_secret    => hiera('CONFIG_NEUTRON_METADATA_PW'),
-      metadata_ip      => force_ip(hiera('CONFIG_KEYSTONE_HOST_URL')),
+      metadata_host    => force_ip(hiera('CONFIG_KEYSTONE_HOST_URL')),
       debug            => hiera('CONFIG_DEBUG_MODE'),
       metadata_workers => hiera('CONFIG_SERVICE_WORKERS'),
     }
