8cbbd4ced6931c730fb14ae783345ce619aefc09
Javier Pena <jpena@redhat.com>
2019-03-14 12:45:02 +0100

    Cast redis port to Integer for use with puppet-redis
    puppet-redis has recently started requesting the port parameter to
    be an integer [1].
    
    [1] - https://github.com/voxpupuli/puppet-redis/commit/8134de7d323f645cccaacbd72c2c4de3e872f6e6
    
    Change-Id: I0346ff4d6c59a99cbec596a68cc9c27a68add415

--- packstack/puppet/modules/packstack/manifests/redis.pp
+++ packstack/puppet/modules/packstack/manifests/redis.pp
@@ -2,7 +2,7 @@ class packstack::redis ()
 {
     create_resources(packstack::firewall, hiera('FIREWALL_REDIS_RULES', {}))
 
-    $redis_port = hiera('CONFIG_REDIS_PORT')
+    $redis_port = Integer(hiera('CONFIG_REDIS_PORT'))
     $redis_host = hiera('CONFIG_REDIS_HOST')
 
     class { '::redis':
