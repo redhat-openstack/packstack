cfdbeb5d029d0b2299ed98ae40a206335c97ff7b
Javier Pena <jpena@redhat.com>
2019-10-04 17:42:56 +0200

    Reduce unnecessary log storage
    Previously we were storing the whole contents of /var/tmp/packstack,
    which includes all Puppet modules and is unnecessarily filling up
    the log server.
    
    Instead, let's store /var/tmp/packstack/latest, where the last
    Packstack execution logs are kept. That is really what we need.
    
    Also added 'ip netns' command to logs to help troubleshooting issues
    with dhcp agent.
    
    Change-Id: Ia138c13c0797d2b83c9e3b4037c4648abf8f7295

--- tools/copy-logs.sh
+++ tools/copy-logs.sh
@@ -45,6 +45,7 @@ function get_diag_commands {
         'iptables -vnL -t mangle'
         'ifconfig -a'
         'ip addr show'
+        'ip netns'
         'lsmod'
         'timeout 15 lsof' # High potential of getting stuck
         'lsof -Pni'
@@ -103,7 +104,7 @@ function get_config_and_logs {
         '/var/log/mariadb'
         '/etc/httpd/conf.d/'
         '/var/log/httpd'
-        '/var/tmp/packstack'
+        '/var/tmp/packstack/latest'
         '/var/tmp/packstack/latest/testrepository.subunit' # So we're copying it
         '/var/log/audit'                                   # to the root of
         '/var/log/secure'                                  # /logs
