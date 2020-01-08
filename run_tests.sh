4f907e26cc46f5528ee8afb7b48a749797c02287
Alfredo Moralejo <amoralej@redhat.com>
2020-01-08 12:11:06 +0100

    Pin gettext to < 3.3.0
    Also pinning gettext as version 3.3.0 is not compatible with the ruby
    version in CentOS7.
    
    Change-Id: I8d226aa2218eafc07119f88eca5be24c3514b717
    (cherry picked from commit 3be5458f4b5a96ddae8d7c1dc733fd5ed3f400d8)

--- run_tests.sh
+++ run_tests.sh
@@ -246,6 +246,7 @@ if [ "${INSTALL_FROM_SOURCE}" = true ]; then
   export GEM_BIN_DIR=/tmp/packstackgems/bin/
   export PUPPETFILE_DIR=/usr/share/openstack-puppet/modules
   export GEM_HOME=/tmp/packstackgems
+  $SUDO gem install gettext -v 3.2.9 --no-ri --no-rdoc
   # puppet_forge 2.2.7 has a dependency on semantic_puppet ~> 1.0
   # which is not compatible with dependency of latest r10k on semantic_puppet ~> 0.1.0
   $SUDO gem install puppet_forge -v '= 2.2.6'
