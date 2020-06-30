e51db4189f80a7a5efa2ec3b5c1b8ed9dc1c9dc7
Alfredo Moralejo <amoralej@redhat.com>
2020-06-30 09:56:37 +0200

    Replace nova_metadata_ip by nova_metadata_host
    nova_metadata_ip was deprecated in favor of nova_metadata_host some time
    ago [1] and after moving ovn driver to main neutron repo, it has been
    removed so we need to use the new parameter.
    
    [1] https://github.com/openstack/networking-ovn/blob/stable/train/networking_ovn/conf/agent/metadata/config.py#L53
    
    Change-Id: Iaba8ff454e01cd5c7455da5d0548d8cfeea3f8d8
    (cherry picked from commit 62a9487c2d2000ef049ac368fd9828bfa706efcb)

--- packstack/puppet/modules/packstack/manifests/neutron/ovn_metadata.pp
+++ packstack/puppet/modules/packstack/manifests/neutron/ovn_metadata.pp
@@ -5,7 +5,7 @@ class packstack::neutron::ovn_metadata ()
     class { '::neutron::agents::ovn_metadata':
       ovn_sb_connection => $ovn_southd,
       shared_secret    => hiera('CONFIG_NEUTRON_METADATA_PW'),
-      metadata_ip      => force_ip(hiera('CONFIG_KEYSTONE_HOST_URL')),
+      metadata_host    => force_ip(hiera('CONFIG_KEYSTONE_HOST_URL')),
       debug            => hiera('CONFIG_DEBUG_MODE'),
       metadata_workers => hiera('CONFIG_SERVICE_WORKERS'),
     }
