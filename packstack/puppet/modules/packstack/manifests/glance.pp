339c8fc04f711cc1751a15029276047c37757baf
Alfredo Moralejo <amoralej@redhat.com>
2020-07-02 10:35:13 +0200

    Stop using glance-registry service
    glance-registry was deprecated some releases ago and it has been fially
    removed in Victoria [1].
    
    This patch stop deploying glance-registry service.
    
    [1] https://review.opendev.org/#/c/738671/
    
    Change-Id: If56beeb503033149115138150b0a865a2d519321

--- packstack/puppet/modules/packstack/manifests/glance.pp
+++ packstack/puppet/modules/packstack/manifests/glance.pp
@@ -12,12 +12,7 @@ class packstack::glance ()
       default => '0.0.0.0',
       # TO-DO(mmagr): Add IPv6 support when hostnames are used
     }
-    # magical hack for magical config - glance option registry_host requires brackets
-    $registry_host = hiera('CONFIG_IP_VERSION') ? {
-      'ipv6'  => '[::0]',
-      default => '0.0.0.0',
-      # TO-DO(mmagr): Add IPv6 support when hostnames are used
-    }
+
     $default_store = hiera('CONFIG_GLANCE_BACKEND') ? {
       'swift' => 'swift',
       default => 'file',
@@ -35,27 +30,10 @@ class packstack::glance ()
 
     class { '::glance::api':
       bind_host           => $bind_host,
-      registry_host       => $registry_host,
       pipeline            => 'keystone',
       database_connection => "mysql+pymysql://glance:${glance_ks_pw}@${glance_mariadb_host}/glance",
       workers             => hiera('CONFIG_SERVICE_WORKERS'),
       stores              => ['file', 'http', 'swift'],
       default_store       => $default_store,
-    }
-
-    class { '::glance::registry::authtoken':
-      www_authenticate_uri => hiera('CONFIG_KEYSTONE_PUBLIC_URL'),
-      auth_url             => hiera('CONFIG_KEYSTONE_ADMIN_URL'),
-      password             => hiera('CONFIG_GLANCE_KS_PW'),
-    }
-
-    class { '::glance::registry::logging':
-      debug => hiera('CONFIG_DEBUG_MODE'),
-    }
-
-    class { '::glance::registry':
-      bind_host           => $bind_host,
-      database_connection => "mysql+pymysql://glance:${glance_ks_pw}@${glance_mariadb_host}/glance",
-      workers             => hiera('CONFIG_SERVICE_WORKERS'),
     }
 }
