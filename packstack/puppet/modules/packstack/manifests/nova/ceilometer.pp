8679c8e4691ee128189aec9fa1e2832bcb2ba184
Alfredo Moralejo <amoralej@redhat.com>
2022-01-05 09:27:06 +0100

    Move to ceilometer::agent::service_credentials
    Class ceilometer::agent::auth has been removed in [1].
    
    [1] https://review.opendev.org/c/openstack/puppet-ceilometer/+/823006
    
    Change-Id: Ic82817f4ab9114e0970cf5256c57cb67c3bea147

--- packstack/puppet/modules/packstack/manifests/nova/ceilometer.pp
+++ packstack/puppet/modules/packstack/manifests/nova/ceilometer.pp
@@ -1,9 +1,9 @@
 class packstack::nova::ceilometer ()
 {
-    class { 'ceilometer::agent::auth':
-      auth_url      => hiera('CONFIG_KEYSTONE_PUBLIC_URL_VERSIONLESS'),
-      auth_password => hiera('CONFIG_CEILOMETER_KS_PW'),
-      auth_region   => hiera('CONFIG_KEYSTONE_REGION'),
+    class { 'ceilometer::agent::service_credentials':
+      auth_url     => hiera('CONFIG_KEYSTONE_PUBLIC_URL_VERSIONLESS'),
+      password     => hiera('CONFIG_CEILOMETER_KS_PW'),
+      region_name  => hiera('CONFIG_KEYSTONE_REGION'),
     }
 
     ensure_packages(['openstack-ceilometer-ipmi'], {'ensure' => 'present'})
