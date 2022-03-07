89fc7184ea0802c28d7f64d820bf8250447df6f2
Takashi Kajinami <tkajinam@redhat.com>
2022-03-07 18:40:42 +0900

    Remove redundant inclusion of apache::listen
    The apache::listen resource is now included automatically and it's not
    necessary to define the resource in composition layer explicitly.
    
    Change-Id: Ia7b80c42fd1d35e4c2b7c60fc53a01e969918f61

--- packstack/puppet/modules/packstack/manifests/apache.pp
+++ packstack/puppet/modules/packstack/manifests/apache.pp
@@ -17,21 +17,7 @@ class packstack::apache ()
 
     if hiera('CONFIG_HORIZON_SSL')  == 'y' {
       ensure_packages(['mod_ssl'], {'ensure' => 'present'})
-      Package['mod_ssl'] -> Class['::apache']
-      apache::listen { '443': }
-    }
-
-    # Keystone port
-    apache::listen { '5000': }
-
-    if hiera('CONFIG_AODH_INSTALL') == 'y' {
-      # Aodh port
-      apache::listen { '8042': }
-    }
-
-    if hiera('CONFIG_CEILOMETER_INSTALL') == 'y' {
-      # Gnocchi port
-      apache::listen { '8041': }
+      Package['mod_ssl'] -> Class['apache']
     }
 }
 
