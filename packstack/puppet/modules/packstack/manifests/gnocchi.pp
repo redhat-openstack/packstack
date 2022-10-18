faf53299410459b754ce84100ed83b4bb00ad2bf
Takashi Kajinami <tkajinam@redhat.com>
2022-10-18 22:07:19 +0900

    Fix dependency between redis and gnocchi services
    This is follow-up of d044e10afc2e8f605a775c62d868cc767a497efd and is
    an attempt to fix the dependency between redis and gnocchi services.
    
    - Use title for resource collector according to the definition in
      puppet-redis. We might want to use the public API such as the redis
      class itself but it makes it difficult to handle the deployment with
      redis and gnocchi running in different nodes.
    
    - Make all gnocchi services dependent on redis.
    
    Change-Id: I70f5ab186b5635b364648f4e4ffb11ba5d328f4c

--- packstack/puppet/modules/packstack/manifests/gnocchi.pp
+++ packstack/puppet/modules/packstack/manifests/gnocchi.pp
@@ -8,7 +8,7 @@ class packstack::gnocchi ()
       $redis_host = hiera('CONFIG_REDIS_HOST_URL')
       $redis_port = hiera('CONFIG_REDIS_PORT')
       $coordination_url = "redis://${redis_host}:${redis_port}"
-      Service<| name == 'redis' |> -> Service<| name == 'gnocchi-metricd' |>
+      Service<| title == 'redis' |> -> Anchor['gnocchi::service::begin']
     } else {
       $coordination_url = ''
     }
