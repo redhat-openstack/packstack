9af0a8e8440b61562c9a33b47613ac2a41254178
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-01-09 14:50:03 +0900

    Clean up unnecessary workaround related to puppetlabs-apache
    Since v5.6.0, mod_packages[1] and mod_libs[2] are automatically set
    to the appropriate value in CentOS >= 8, so we don't need to set these
    parameters explicitly.
    
    Also, the mod_ssl package is now installed automatically when ssl is
    enabled in vhost.
    
    [1] https://github.com/puppetlabs/puppetlabs-apache/commit/d346b76d1
    [2] https://github.com/puppetlabs/puppetlabs-apache/commit/f31e392a6
    
    Change-Id: I28feb2b5d4cde4c430806e5fc8b9b7c98695214a

--- packstack/puppet/modules/packstack/manifests/apache.pp
+++ packstack/puppet/modules/packstack/manifests/apache.pp
@@ -1,23 +1,5 @@
 class packstack::apache ()
 {
-    # Use python3 for mod_wsg in fedora
-    if ($::operatingsystem == 'Fedora') or ($::osfamily == 'RedHat' and Integer.new($::operatingsystemmajrelease) > 7)  {
-      class { 'apache':
-        mod_packages  => merge($::apache::params::mod_packages, {
-          'wsgi' => 'python3-mod_wsgi',
-        }),
-        mod_libs      => merge($::apache::params::mod_libs, {
-          'wsgi' => 'mod_wsgi_python3.so',
-        })
-      }
-    }else{
-      class { 'apache':
-      }
-    }
-
-    if lookup('CONFIG_HORIZON_SSL') == 'y' {
-      ensure_packages(['mod_ssl'], {'ensure' => 'present'})
-      Package['mod_ssl'] -> Class['apache']
-    }
+    class { 'apache': }
 }
 
