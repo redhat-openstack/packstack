758af27ff56cd20f810c51331259937cd3e4eae7
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-10-26 23:11:37 +0900

    Drop unused variables
    The variables defined in core neutron class are not at all used.
    
    Change-Id: I38dbb37c920a87cdf54e1843a63ff2bb5b801030

--- packstack/puppet/modules/packstack/manifests/neutron.pp
+++ packstack/puppet/modules/packstack/manifests/neutron.pp
@@ -1,9 +1,3 @@
 class packstack::neutron ()
 {
-    $neutron_db_host         = lookup('CONFIG_MARIADB_HOST_URL')
-    $neutron_db_name         = lookup('CONFIG_NEUTRON_L2_DBNAME')
-    $neutron_db_user         = 'neutron'
-    $neutron_db_password     = lookup('CONFIG_NEUTRON_DB_PW')
-    $neutron_sql_connection  = "mysql+pymysql://${neutron_db_user}:${neutron_db_password}@${neutron_db_host}/${neutron_db_name}"
-    $neutron_user_password   = lookup('CONFIG_NEUTRON_KS_PW')
 }
